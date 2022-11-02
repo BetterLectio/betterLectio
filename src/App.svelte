f4004d6d06c1292e0574e1c5bc3bd110bddddecb
victor <victordj587@gmail.com>
2022-11-02 13:54:36 +0100

    init svelte app

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Counter from "./lib/Counter.svelte";
+</script>
+
+<main>
+    <p>Work in progress...</p>
+    <p>The built upon the Svelte framework powered by Vite!</p>
+</main>
