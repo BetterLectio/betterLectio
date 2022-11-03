1906c90ad570e4b67ab00918eaf846bab0b9e2ce
victor <victordj587@gmail.com>
2022-11-03 16:41:42 +0100

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,2 @@
-<h1 class="text-3xl font-bold underline">Welcome to Better Lectio</h1>
+<h1 class="text-3xl font-bold">Welcome to Better Lectio</h1>
 <p>Better Lectio is a web app that makes it easier to use Lectio.</p>
-<hr />
-<p>It's currently in development, so it's not ready for use yet.</p>
-<p>But you can still try it out by logging in with your Lectio account.</p>
-<hr />
-<form action="auth">
-  <button type="submit" class="btn btn-primary">Login</button>
-</form>
