95426f5933a0367960047d0fd387e5be4eee4ec2
victor <victordj587@gmail.com>
2022-11-03 19:42:42 +0100

    eh

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,21 +1,25 @@
-<script>
-</script>
-
 <h1>Log ind med din Lectio-konto</h1>
 <form action="login">
-    <label for="username">Brugernavn</label>
+    <script src="https://cdn.jsdelivr.net/gh/Asguho/LectioJS/api.js"></script>
+    <select class="select select-bordered select-primary w-full max-w-xs">
+        <option disabled selected>Vælg skole</option>
+    </select>
+    <br />
     <input
         type="text"
         name="username"
         id="username"
         class="input input-bordered input-primary w-full max-w-xs"
+        placeholder="Brugernavn"
     />
-    <label for="password">Kodeord</label>
+    <br />
     <input
         type="password"
         name="password"
         id="password"
         class="input input-bordered input-primary w-full max-w-xs"
+        placeholder="Kodeord"
     />
+    <br />
     <button type="submit" class="btn btn-primary">Log ind</button>
 </form>
