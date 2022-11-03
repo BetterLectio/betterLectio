29c50f6904b3a1c576e1c9b02bcb2ebfc2cefd4d
victor <victordj587@gmail.com>
2022-11-03 19:09:15 +0100

    Co-authored-by: Asguho <Asguho@users.noreply.github.com>

---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<h1>Log ind med din Lectio-konto</h1>
+<form action="login">
+    <label for="username">Brugernavn</label>
+    <input
+        type="text"
+        name="username"
+        id="username"
+        class="input input-bordered input-primary w-full max-w-xs"
+    />
+    <label for="password">Kodeord</label>
+    <input
+        type="password"
+        name="password"
+        id="password"
+        class="input input-bordered input-primary w-full max-w-xs"
+    />
+    <button type="submit" class="btn btn-primary">Log ind</button>
+</form>
