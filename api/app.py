976d24de1eb1f6f6546d6d80d373b31eb5c731a5
jona799t <jonathan.b.skettrup@gmail.com>
2022-11-04 16:55:09 +0100

    Fixed CORS error

--- api/app.py
+++ api/app.py
@@ -1,9 +1,11 @@
 import lectio
 import json
 import base64
-from flask import Flask, jsonify, request, CORS
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"*": {"origins": "*"}})
 
 @app.route("/")
 def index():
