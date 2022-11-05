d2f11cec70fc8b43da4a755e635b46ec15fa8d32
jona799t <jonathan.b.skettrup@gmail.com>
2022-11-05 14:07:30 +0100

    Skema er her men mangler timer

--- src/routes/skema/+page.svelte
+++ src/routes/skema/+page.svelte
@@ -1,4 +1,36 @@
 <script>
+    import FullCalendar from 'svelte-fullcalendar';
+    import timeGridPlugin from '@fullcalendar/timegrid';
+    import daLocale from '@fullcalendar/core/locales/da';
+    
+    let options = {
+        initialView: 'timeGridWeek',
+        plugins: [ timeGridPlugin ],
+        weekNumberCalculation: "ISO",
+        locale: daLocale,
+        weekends: false,
+        
+        slotMinTime: "08:00:00",
+        slotMaxTime: "18:00:00",
+
+        slotDuration: "00:30:00",
+
+        dateClick: (event) => alert('date click! ' + event.dateStr),
+        events: [
+            {
+                title: 'Ting sker',
+                defaultAllDay: true,
+                date: "2022-11-02"
+            },
+            { 
+                title: 'event 1', 
+                start: '2022-11-01T14:30:00',
+                end: '2022-11-01T15:30:00',
+            },
+        ],
+        timeFormat: 'H(:mm)' // uppercase H for 24-hour clock
+    };
+
     let skema = '';
 
     let checked = false;
@@ -17,11 +49,19 @@
             `https://better-lectio-flask-backend.vercel.app/skema?cookie=${localStorage.getItem("authentication")}`
         );
         skema = await response.json()
+
+    }
+    function toggleWeekends() {
+        options = {
+        ...options,
+        weekends: !options.weekends
+        };
     }
 </script>
 
 <h1 class="text-3xl font-bold">Skema</h1>
 <br />
+<FullCalendar {options} />
 <body use:checkIfAuthed>
     {#if skema != ''}
         <div>
