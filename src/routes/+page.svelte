3ab2edef2e4a9e1882598355efe4ac027e24cc0b
victor <victordj587@gmail.com>
2022-11-06 09:33:23 +0100

    added autologin to route /

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,19 @@
+<script>
+  async function checkIfAuthed() {
+    if (localStorage.getItem("authentication") == null) {
+    } else {
+      const response = await fetch(`https://better-lectio-flask-backend.vercel.app/check-cookie?cookie=${localStorage.getItem("authentication")}`);
+      let jsonRes = await response.json();
+      console.log(jsonRes["valid"]);
+      if (jsonRes["valid"] == true) {
+        console.log("Redirect")
+        window.location.href = "/hjem";
+      }
+    }
+  }
+</script>
 
-
-<h1 class="text-3xl font-bold">Velkommen til Better Lectio</h1>
+<h1 use:checkIfAuthed class="text-3xl font-bold">Velkommen til Better Lectio</h1>
 <p>Better Lectio er en webapp, der gør det nemmere at bruge Lectio.</p>
 <p>For at bruge Better Lectio skal du logge ind med din Lectio konto.</p>
 <br />
