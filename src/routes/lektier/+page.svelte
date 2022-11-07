9b255263c0ac545246fe164b3cfca7e265a9bdeb
jona799t <jonathan.b.skettrup@gmail.com>
2022-11-07 14:14:33 +0100

    Lektier med beskrivelse

--- src/routes/lektier/+page.svelte
+++ src/routes/lektier/+page.svelte
@@ -1,4 +1,10 @@
 <script>
+    /**
+     TODO:
+        - Gør så man kan downloade filer uden at blive redirectet til modul siden
+        - Måske gør så man får al teksten og derfor ikke behøver at klikke på lektien
+     */
+
     let lektier = [];
     async function checkIfAuthed() {
     if (localStorage.getItem("authentication") == null) {
@@ -26,11 +32,17 @@
 </script>
 <body use:checkIfAuthed>
     <h1 class="text-3xl font-bold">Lektier</h1>
+    <h3 class="text-xl">Her kan du se dine lektier for de kommende 14 dage</h3>
     <br/>
     <ul class="menu bg-base-100 w-full p-2 rounded-box drop-shadow-xl md:w-fit">
         {#each lektier as lektie}
             <li class="">
-                <a href="https://www.lectio.dk/{lektie.lektier.link}"><p>{lektie.aktivitet.hold, lektie.aktivitet.lokale}</p></a>
+                <a href="/modul?absid={lektie.aktivitet.absid}">
+                    <div>
+                        <p><span class="font-bold">{(lektie.aktivitet.navn != null) ? lektie.aktivitet.navn + " · " : ""}{lektie.aktivitet.hold}</span> ({lektie.aktivitet.tidspunkt})</p>
+                        <p>{lektie.lektier.beskrivelse}</p>
+                    </div>
+                </a>
             </li>
         {/each}
     </ul>
