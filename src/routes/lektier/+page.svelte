3deaac12a1c2ed6ecdea5ef63621e31f7df6dcb0
Asguho <78410939+Asguho@users.noreply.github.com>
2022-11-07 10:30:31 +0100

    added the ability to view lektier

--- src/routes/lektier/+page.svelte
+++ src/routes/lektier/+page.svelte
@@ -1,2 +1,36 @@
-<h1 class="text-3xl font-bold">Lektier</h1>
-<br />
+<script>
+    let lektier = [];
+    async function checkIfAuthed() {
+    if (localStorage.getItem("authentication") == null) {
+        console.log("Redirect")
+        window.location.href = "/auth";
+    } else {
+        const response = await fetch(
+            `https://better-lectio-flask-backend.vercel.app/check-cookie?cookie=${localStorage.getItem("authentication")}`
+        );
+        if (await response.json()["valid"] == false) {
+            console.log("Redirect")
+            window.location.href = "/auth";
+        }
+    }
+
+    await fålektier()
+    }
+    async function fålektier() {
+        const response = await fetch(
+            `https://better-lectio-flask-backend.vercel.app/lektier?cookie=${localStorage.getItem("authentication")}`
+        );
+        lektier = await response.json()
+        console.log(lektier);
+    }
+</script>
+<body use:checkIfAuthed>
+    <h1 class="text-3xl font-bold">Lektier</h1>
+    <br/>
+    <ul class="menu bg-base-100 w-full p-6 rounded-box drop-shadow-xl md:w-fit">
+        {#each lektier as lektie}
+            <li class="">{lektie.aktivitet.hold, lektie.aktivitet.lokale}</li>
+        {/each}
+    </ul>
+</body>
+
