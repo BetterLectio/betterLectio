6c3079e7351f6f7431d71db54aa0f37e8e9bf632
jona799t <jonathan.b.skettrup@gmail.com>
2022-11-08 18:50:58 +0100

    Lidt design på beskeder

--- src/routes/beskeder/+page.svelte
+++ src/routes/beskeder/+page.svelte
@@ -7,18 +7,36 @@
     import { get } from "../../components/http.js"
 
     let beskeder = [];
+    let ready = false;
     async function fåBeskeder() {
         beskeder = await get(
             `/beskeder`
         );
         console.log(beskeder);
+        ready = true
     }
 </script>
 
 <body use:fåBeskeder>
     <h1 class="text-3xl font-bold">Beskeder</h1>
     <br/>
-    <ul class="menu bg-base-100 w-full p-2 rounded-box drop-shadow-xl md:w-fit">
-        {JSON.stringify(beskeder)}
-    </ul>
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    {#if ready}
+        <btn class="btn btn-primary">Place holder for aktiv</btn>
+        {#each beskeder.beskedMuligheder as beskedMulighed}
+            <!-- Få mapper til at åbne når man klikker på dem-->
+            <btn class="btn">{beskedMulighed.name}</btn>
+        {/each}
+        <ul class="menu bg-base-100 w-full p-2 rounded-box drop-shadow-xl md:w-fit">
+            {#each beskeder.beskeder as besked}
+            <li>
+                <a href="/besked?id={besked.message_id}">
+                    <div>
+                        <p>{JSON.stringify(besked)}</p>
+                    </div>
+                </a>
+            </li>
+            {/each}
+        </ul>
+    {/if}
 </body>
