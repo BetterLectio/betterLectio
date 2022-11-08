1d49caa8cdf4f862795b1efd2c74adc626b17eb0
jona799t <jonathan.b.skettrup@gmail.com>
2022-11-08 09:12:11 +0100

    Nu med opgaver

---
+++ src/routes/opgave/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { page } from '$app/stores';
+    import { get } from "../../components/http.js"
+
+    const exerciseid = $page.url.searchParams.get('exerciseid');
+</script>
+
+<p>exerciseid={exerciseid}</p>
--- src/routes/opgaver/+page.svelte
+++ src/routes/opgaver/+page.svelte
@@ -1,2 +1,43 @@
-<h1 class="text-3xl font-bold">Opgaver</h1>
-<br />
+<script>
+    import { get } from "../../components/http.js"
+
+    let opgaver = [];
+    let ikkeAfleveretOpgaver = [];
+    let afleveretOpgaver = [];
+    let selected = 0;
+    let ready = false;
+    async function fåOpgaver() {
+        opgaver = await get("/opgaver")
+        opgaver.forEach(opgave => {
+            if (opgave.status == "Afleveret") {
+                afleveretOpgaver.push(opgave)
+            } else {
+                ikkeAfleveretOpgaver.push(opgave)
+            }
+            
+        });
+        
+        ready = true;
+    }
+</script>
+
+<body use:fåOpgaver>
+    <h1 class="text-3xl font-bold">Opgaver</h1>
+    <br />
+    <a class="btn btn-primary">Ikke afleveret opgaver</a>
+    <a class="btn">Afleveret opgaver</a>
+    {#if ready}
+        <ul class="menu bg-base-100 w-full p-2 rounded-box drop-shadow-xl md:w-fit">
+            {#each ikkeAfleveretOpgaver as opgave}
+                <li class="">
+                    <a href="/opgave?exerciseid={opgave.exerciseid}">
+                        <div>
+                            <p><span class="font-bold">{opgave.opgavetitel} · {opgave.hold}</span> ({opgave.frist})</p>
+                            <p>{opgave.opgavenote}</p>
+                        </div>
+                    </a>
+                </li>
+            {/each}
+        </ul>
+    {/if}
+</body>
