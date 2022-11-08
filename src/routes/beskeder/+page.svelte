a35b14cf9100fcbed2e750c6b2d0fbb9f392e3b2
jona799t <jonathan.b.skettrup@gmail.com>
2022-11-08 18:42:11 +0100

    http.js fix + beskeder

--- src/routes/beskeder/+page.svelte
+++ src/routes/beskeder/+page.svelte
@@ -1,2 +1,24 @@
-<h1 class="text-3xl font-bold">Beskeder</h1>
-<br />
+<script>
+    /**
+     TODO:
+        - Gør så man kan downloade filer uden at blive redirectet til modul siden
+        - Måske gør så man får al teksten og derfor ikke behøver at klikke på lektien
+     */
+    import { get } from "../../components/http.js"
+
+    let beskeder = [];
+    async function fåBeskeder() {
+        beskeder = await get(
+            `/beskeder`
+        );
+        console.log(beskeder);
+    }
+</script>
+
+<body use:fåBeskeder>
+    <h1 class="text-3xl font-bold">Beskeder</h1>
+    <br/>
+    <ul class="menu bg-base-100 w-full p-2 rounded-box drop-shadow-xl md:w-fit">
+        {JSON.stringify(beskeder)}
+    </ul>
+</body>
