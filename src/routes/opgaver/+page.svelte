d8db27f50eeb8d05de3d47592baccfed6980535e
victor <victordj587@gmail.com>
2022-11-08 10:40:52 +0100

    added ui support for firefox browser

--- src/routes/opgaver/+page.svelte
+++ src/routes/opgaver/+page.svelte
@@ -41,14 +41,13 @@
 </script>
 
 <body use:fåOpgaver>
-    <h1 class="text-3xl font-bold">Opgaver</h1>
-    <br />
+    <h1 class="text-3xl font-bold my-4">Opgaver</h1>
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     <btn class={ikkeAfleveredeOpgaverClass} on:click={changeView}>Ikke afleveret opgaver</btn>
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     <btn class={afleveredeOpgaverClass} on:click={changeView}>Afleverede opgaver</btn>
     {#if ready}
-        <ul class="menu bg-base-100 w-full p-2 rounded-box drop-shadow-xl md:w-fit">
+        <ul class="menu bg-base-100 w-full p-2 my-4 rounded-box drop-shadow-xl md:w-fit">
             {#each opgaver as opgave}
                 <li class="">
                     <a href="/opgave?exerciseid={opgave.exerciseid}">
