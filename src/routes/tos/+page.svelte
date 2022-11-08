420ecc075342b1ddc1f7830a95c4c8deeb3ff3a3
victor <victordj587@gmail.com>
2022-11-08 18:21:04 +0100

    added basic tos IDK

--- src/routes/tos/+page.svelte
+++ src/routes/tos/+page.svelte
@@ -1,2 +1,44 @@
-<h1 class="text-3xl font-bold">Terms of service</h1>
-<br />
+<h1 class="text-3xl font-bold">Servicevilkår & Privatlivspolitik</h1>
+<h2 class="text-2xl font-bold">1. Indledning</h2>
+
+<p class="my-4">
+    Vi er glade for, at du har valgt at bruge vores tjenester. Vi er også glade
+    for, at du har valgt at læse vores servicevilkår og privatlivspolitik. Vi
+    har skrevet dem for at beskytte dig og os selv. Vi håber, at du vil læse dem
+    grundigt igennem, så du ved, hvad du kan forvente af os og hvad vi forventer
+    af dig.
+</p>
+
+<h2 class="text-2xl font-bold">2. Servicevilkår</h2>
+
+<p class="my-4">
+    Vi er ikke ansvarlige noget som helst, der sker med dig eller dine ting, når
+    du bruger vores tjenester. Vi er ikke ansvarlige for, at du ikke kan bruge
+    vores tjenester. Vi er ikke ansvarlige for, at infomationen på vores
+    hjemmeside er forkert. Vi er ikke ansvarlige for, noget der sker med dig,
+    mens du bruger vores tjenester. Vi er ikke ansvarlige for, noget der sker med dig,
+    fordi du bruger/har brugt vores tjenester. Vi er ikke ansvarlige for, at du ikke kan
+    benytte alle funktioner på vores hjemmeside.
+</p>
+
+<h2 class="text-2xl font-bold">3. Privatlivspolitik</h2>
+
+<p class="my-4">
+    Vi gemmer ikke noget som helst om dig hos os, det er ikke nødvendigt. dog er
+    det en mulighed at din browser kan i nogle tilfælde opbevare en Base64
+    kryteret version af dit brugernavn og skoleid m.m. for at gøre det nemmere
+    for dig at logge ind. vi har ikke adgang til denne information og kan ikke
+    se den på vores servere.
+</p>
+
+<h2 class="text-2xl font-bold">5. Kontakt</h2>
+
+<p class="my-4">
+    Hvis du har spørgsmål til vores servicevilkår eller privatlivspolitik, så er
+    du velkommen til at kontakte os på github: <a
+        href="https://github.com/victorDigital/betterLectio/issues/new/choose"
+        >her</a
+    >
+</p>
+
+
