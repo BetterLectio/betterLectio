642ab586dd1d8536d34ef33726984282784d9163
victor <victordj587@gmail.com>
2022-11-09 12:03:42 +0100

    added custom lectio theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,5 +13,22 @@ module.exports = {
       },
     },
   },
-  plugins: [require("daisyui"), require("@tailwindcss/forms"), require('@tailwindcss/line-clamp')], 
+  daisyui: {
+    themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter",
+      {
+        lectio: {
+          "primary": "#1b5c9e",
+          "secondary": "#1b5c9e",
+          "accent": "#B20C00",
+          "neutral": "#b8c3cc",
+          "base-100": "#fff",
+          "info": "#53C0F3",
+          "success": "#339900",
+          "warning": "#F3CC30",
+          "error": "#B20C00",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui"), require("@tailwindcss/forms"), require('@tailwindcss/line-clamp')],
 };
