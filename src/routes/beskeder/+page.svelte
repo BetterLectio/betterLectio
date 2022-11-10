f134c9962600cf49c79d9d031e408efc9e1961bd
victor <victordj587@gmail.com>
2022-11-10 10:12:23 +0100

    added ui change to beseder page

--- src/routes/beskeder/+page.svelte
+++ src/routes/beskeder/+page.svelte
@@ -18,22 +18,24 @@
 </script>
 
 <body use:fåBeskeder>
-    <h1 class="text-3xl font-bold">Beskeder</h1>
-    <br/>
+    <h1 class="text-3xl font-bold mb-4">Beskeder</h1>
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     {#if ready}
-        <btn class="btn btn-primary">Place holder for aktiv</btn>
+    <div class="btn-group overflow-x-scroll w-full mb-4">
+        <btn class="btn btn-active">Place holder for aktiv</btn>
         {#each beskeder.beskedMuligheder as beskedMulighed}
             <!-- Få mapper til at åbne når man klikker på dem-->
             <btn class="btn">{beskedMulighed.name}</btn>
         {/each}
+    </div>
         <ul class="menu bg-base-100 w-full p-2 rounded-box drop-shadow-xl md:w-fit">
             {#each beskeder.beskeder as besked}
             <li>
-                <a href="/besked?id={besked.message_id}">
+                <a class="block" href="/besked?id={besked.message_id}">
                     <div>
-                        <!-- INDSÆT PROFIL BILLEDE AF DEN SOM SENDTE BESKEDEN -->
-                        <p>{JSON.stringify(besked)}</p>
+                        <p part="afsender" class="btn btn-xs w-full">{besked.førsteBesked} ({besked.ændret})</p>
+                        <p part="emne" class="text-lg font-bold">{besked.emne}</p>
+
                     </div>
                 </a>
             </li>
