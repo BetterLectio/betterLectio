034375e566d2235ad2c2cf213b454c4b20dacc5e
victor <victordj587@gmail.com>
2022-11-17 19:26:34 +0100

    added mødetid to værktøjer

--- src/routes/værktøjer/+page.svelte
+++ src/routes/værktøjer/+page.svelte
@@ -3,8 +3,28 @@
 <ul class="menu bg-base-100 w-full p-2 rounded-box drop-shadow-xl">
   <li class="flex">
     <a class="flex-row " href="fri">
-      <p part="afsender" class="btn btn-xs w-fit">NY!</p>
+      <p part="afsender" class="btn btn-xs w-10 ">NY!</p>
       <p part="emne" class="text-lg font-bold">Hvornår har jeg fri?</p>
     </a>
   </li>
+  <li class="flex">
+    <a class="flex-row " href="mødetid">
+      <p part="afsender" class="btn btn-xs w-10 btn-warning">BETA</p>
+      <p part="emne" class="text-lg font-bold">Hvornår møder jeg samme tid som</p>
+    </a>
+  </li>
+  <!--    FOR FUTURE USE NOT READY FOR PROD
+    <li class="flex">
+      <a class="flex-row " href="#">
+        <p part="emne" class="text-lg font-bold">Elev status</p>
+        <p part="afsender" class="btn btn-xs w-fit btn-success">better Lectio Excluisve</p>
+      </a>
+    </li>
+    <li class="flex">
+      <a class="flex-row " href="#">
+        <p part="emne" class="text-lg font-bold">Skole community</p>
+        <p part="afsender" class="btn btn-xs w-fit btn-success">better Lectio Excluisve</p>
+      </a>
+    </li>
+  -->
 </ul>
