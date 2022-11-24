ba5aa8645fc6795f6439f63464cd990d7b56ef80
Asguho <78410939+Asguho@users.noreply.github.com>
2022-11-24 17:46:01 +0100

    fixed clientside crash

--- src/components/localStore.js
+++ src/components/localStore.js
@@ -7,32 +7,19 @@ export const localStore = (key, initial) => {
   const toString = (value) => JSON.stringify(value, null, 2); // helper function
   const toObj = JSON.parse; // helper function
 
-  if (browser) {
-    if (localStorage.getItem(key) === null) {
-      // item not present in local storage
-      localStorage.setItem(key, toString(initial)); // initialize local storage with initial value
-    }
+  if (localStorage.getItem(key) === null) {
+    // item not present in local storage
+    localStorage.setItem(key, toString(initial)); // initialize local storage with initial value
+  }
 
-    const saved = toObj(localStorage.getItem(key)); // convert to object
-  }
-  if (browser) {
-    const { subscribe, set, update } = writable(saved); // create the underlying writable store
-    return {
-      subscribe,
-      set: (value) => {
-        localStorage.setItem(key, toString(value)); // save also to local storage as a string
-        return set(value);
-      },
-      update,
-    };
-  } else {
-    const { subscribe, set, update } = writable(initial); // create the underlying writable store
-    return {
-      subscribe,
-      set: (value) => {
-        return set(value);
-      },
-      update,
-    };
-  }
+  const saved = toObj(localStorage.getItem(key)); // convert to object
+  const { subscribe, set, update } = writable(saved); // create the underlying writable store
+  return {
+    subscribe,
+    set: (value) => {
+      localStorage.setItem(key, toString(value)); // save also to local storage as a string
+      return set(value);
+    },
+    update,
+  };
 };
