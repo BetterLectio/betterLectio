c8b69fca7fa1434cdc7720f39f1fdd8076aaf666
Asguho <78410939+Asguho@users.noreply.github.com>
2022-11-28 15:46:27 +0100

    added placeholder i fravær

--- src/routes/fravær/+page.svelte
+++ src/routes/fravær/+page.svelte
@@ -1,1 +1,41 @@
-<h1 class="mb-4 text-3xl font-bold">Fravær</h1>
+<script>
+  import { fravaer } from "../../components/store";
+  import { get } from "../../components/http";
+  const CokieInfo = async () => {
+    if (!localStorage.getItem("authentication")) {
+      console.log("Redirect");
+      window.location.href = "/auth";
+    } else {
+      let decodedCookie = atob(localStorage.getItem("authentication"));
+      cookie = JSON.parse(decodedCookie);
+      return {
+        user: cookie["LastLoginUserName"],
+        school: cookie["LastLoginExamno"],
+        userid: cookie["LastLoginElevId"]
+      }
+    }
+  };
+  let cookie;
+  CokieInfo().then((data) => {
+    cookie = data;
+  });
+  get("/fravaer").then((data) => {
+    $fravaer = (data);
+  });
+
+</script>
+
+<h1 class="mb-4 text-3xl font-bold">Fravær - Work in progress</h1>
+{#if $fravaer}
+  <p>{JSON.stringify($fravaer)}</p>
+{/if}
+
+{#if cookie}
+  <a href="https://www.lectio.dk/lectio/681/subnav/fravaerelev.aspx?elevid={cookie.userid}" class="btn btn-primary">Åben i lectio</a>
+{:else}
+  <p class="mb-4">loading...</p>
+{/if}
+
+  
+
+
