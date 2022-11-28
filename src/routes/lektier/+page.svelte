6cbae2e50bf63fb5b3d6ca12d58b7c1174e8d62b
Asguho <78410939+Asguho@users.noreply.github.com>
2022-11-28 16:02:44 +0100

    tried fixing bug with lektier

--- src/routes/lektier/+page.svelte
+++ src/routes/lektier/+page.svelte
@@ -16,21 +16,23 @@
   <h1 class="text-3xl font-bold">Lektier</h1>
   <h3 class="mb-4 text-xl">Her kan du se dine lektier for de kommende 14 dage</h3>
   <ul class="menu rounded-box w-full bg-base-100 p-2 drop-shadow-xl md:w-fit">
-    {#each $lektier as lektie}
-      <li class="">
-        <a href="/modul?absid={lektie.aktivitet.absid}">
-          <div>
-            <p>
-              <span class="font-bold"
-                >{lektie.aktivitet.navn != null ? lektie.aktivitet.navn + " · " : ""}{lektie.aktivitet
-                  .hold}</span
-              >
-              ({lektie.aktivitet.tidspunkt})
-            </p>
-            <p>{lektie.lektier.beskrivelse}</p>
-          </div>
-        </a>
-      </li>
-    {/each}
+    {#if $lektier}
+      {#each $lektier as lektie}
+        <li class="">
+          <a href="/modul?absid={lektie.aktivitet.absid}">
+            <div>
+              <p>
+                <span class="font-bold"
+                  >{lektie.aktivitet.navn != null ? lektie.aktivitet.navn + " · " : ""}{lektie.aktivitet
+                    .hold}</span
+                >
+                ({lektie.aktivitet.tidspunkt})
+              </p>
+              <p>{lektie.lektier.beskrivelse}</p>
+            </div>
+          </a>
+        </li>
+      {/each}
+    {/if}
   </ul>
 </body>
