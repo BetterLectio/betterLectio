38ecde42c29974e7e0eb1418aea8f78abff4e3b6
Asguho <78410939+Asguho@users.noreply.github.com>
2022-11-29 13:23:50 +0100

    fixed problem with /hjem

--- src/components/localStore.js
+++ src/components/localStore.js
@@ -4,11 +4,11 @@ export const localStore = (key) => {
   // receives the key of the local storage and an initial value
 
   const toString = (value) => JSON.stringify(value, null, 2); // helper function
-  const toObj = JSON.parse; // helper function
+  const toObj = (value) => (value ? JSON.parse(value) : null); // helper function
 
   const saved = toObj(localStorage.getItem(key)); // convert to object
 
-  const { subscribe, set, update } = writable(saved); // create the underlying writable store
+  const { subscribe, set, update } = writable(saved || {}); // create the underlying writable store
   return {
     subscribe,
     set: (value) => {
