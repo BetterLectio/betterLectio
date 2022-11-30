8eaba2b0645b049ff813b999e2b39a8d532a2465
Asguho <78410939+Asguho@users.noreply.github.com>
2022-11-30 12:22:19 +0100

    added % to fravær

--- src/routes/fravær/+page.svelte
+++ src/routes/fravær/+page.svelte
@@ -22,27 +22,27 @@
   let samletFravaer = null;
   get("/fravaer").then((data) => {
     $fravaer = data;
-    $fravaer.generalt.forEach(element=>{
+    $fravaer.generalt.forEach((element) => {
       if (element.hold == "Samlet") {
-        samletFravaer = parseFloat(element.fravær_procent)
+        samletFravaer = parseFloat(element.fravær_procent);
       }
-    })
+    });
   });
 </script>
 
 <h1 class="mb-4 text-3xl font-bold">Fravær - Work in progress</h1>
 {#if $fravaer}
   {#if samletFravaer == 0}
-      <p>Du har intet fravær</p>
-    {:else}
-    <p>{JSON.stringify(samletFravaer)}</p>
+    <p>Du har intet fravær</p>
+  {:else}
+    <p>{JSON.stringify(samletFravaer)}%</p>
   {/if}
 {/if}
 
 {#if cookie}
   <a
     href="https://www.lectio.dk/lectio/681/subnav/fravaerelev.aspx?elevid={cookie.userid}"
-    class="btn btn-primary">Åben i lectio</a
+    class="btn-primary btn">Åben i lectio</a
   >
 {:else}
   <p class="mb-4">loading...</p>
