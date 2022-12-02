6e4a97bfd0f69900c9c0354b194d28fba2f3995b
Asguho <78410939+Asguho@users.noreply.github.com>
2022-12-02 21:14:59 +0100

    updated +page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,27 @@
 <script>
-  async function checkIfAuthed() {
-    if (localStorage.getItem("authentication") != null) {
-      window.location.href = "/forside";
-    } else {
-      console.log("Not logged in");
+  function tryLoginInWithCookie() {
+    if (localStorage.getItem("authentication")) {
+      fetch(`https://better-lectio-flask-backend.vercel.app/check-cookie`, {
+        headers: {
+          "lectio-cookie": localStorage.getItem("authentication"),
+        },
+      }).then((res) => {
+        res.json().then((data) => {
+          if (data && data.valid) {
+            console.log("Logged in with cookie");
+            window.location.href = "/forside";
+          } else {
+            console.log("Cookie not valid");
+          }
+        });
+      });
     }
   }
+  tryLoginInWithCookie();
 </script>
 
 <div>
-  <h1 use:checkIfAuthed class="text-3xl font-bold">Velkommen til Better Lectio</h1>
+  <h1 class="text-3xl font-bold">Velkommen til Better Lectio</h1>
   <p>Better Lectio er en webapp, der gør det nemmere at bruge Lectio.</p>
   <p>For at bruge Better Lectio skal du logge ind med din Lectio konto.</p>
   <br />
