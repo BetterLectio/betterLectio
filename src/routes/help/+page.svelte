0c7b14e885a76ad0b10bcaf7facfde763c0abeb6
Asguho <78410939+Asguho@users.noreply.github.com>
2022-12-03 17:10:21 +0100

    fixed style on /help

--- src/routes/help/+page.svelte
+++ src/routes/help/+page.svelte
@@ -1,23 +1,22 @@
-<h1 class="mb-4 text-3xl font-bold">Hjælp</h1>
-<h2 class="text-2xl">Hvad er Better Lectio?</h2>
-<p>
+<h1 class=" text-3xl font-bold">Hjælp</h1>
+<h2 class="mt-2 text-2xl font-bold">Hvad er Better Lectio?</h2>
+<p class="mb-2">
   Better Lectio er en webapp, der gør det nemmere at bruge Lectio. Better Lectio tager kontakt til lectio
-  API'en som er srevet af os og er tilgængelig for alle.
+  API'en som er skrevet af os og er tilgængelig for alle.
 </p>
-<br />
-<h2 class="text-2xl">Hvordan virker det?</h2>
+<h2 class="mt-2 text-2xl font-bold">Hvordan virker det?</h2>
 <p />
-
-<br />
-<h2 class="text-2xl">Links</h2>
-<ul>
+<h2 class="mt-2 text-2xl font-bold">Links</h2>
+<ul class="mb-2">
   <li>
-    <a href="https://github.com/betterlectio/betterLectio">GitHub</a>
+    <a class="link" href="https://github.com/betterlectio/betterLectio">GitHub</a>
   </li>
   <li>
-    <a href="https://github.com/betterlectio/LectioJS">LectioJS</a>
+    <a class="link" href="https://github.com/betterlectio/LectioJS">LectioJS</a>
   </li>
   <li>
-    <a href="https://github.com/betterlectio/BetterLectio-Flask-Backend">Better Lectio Flask Backend</a>
+    <a class="link" href="https://github.com/betterlectio/BetterLectio-Flask-Backend"
+      >Better Lectio Flask Backend</a
+    >
   </li>
 </ul>
