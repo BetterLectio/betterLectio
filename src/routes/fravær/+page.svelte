aa0bacb9edc5158238d0e8191bc9979171617cbb
Johnny JTH <johanohly@gmail.com>
2022-12-05 15:47:42 +0100

    Pænere fravær table
    Matcher table style fra opgave tablen

--- src/routes/fravær/+page.svelte
+++ src/routes/fravær/+page.svelte
@@ -1,8 +1,7 @@
 <script>
   import { fravaer } from "../../components/store";
   import { get } from "../../components/http";
-  import Table from "../../components/Table.svelte";
-  import table from "markdown-it/lib/rules_block/table";
+
   let samletFravaer = null;
   get("/fravaer").then((data) => {
     $fravaer = data;
@@ -14,33 +13,33 @@
   });
 </script>
 
-<h1 class="mb-4 text-3xl font-bold">Fravær</h1>
+<h1 class="text-3xl font-bold">Fravær</h1>
 {#if $fravaer}
   {#if samletFravaer == 0}
-    <p class="m-4">Du har intet fravær</p>
+    <p>Du har intet fravær</p>
   {:else}
-    <p class="m-4">Du har {samletFravaer}% fravær</p>
+    <p>Du har {samletFravaer}% fravær</p>
+    <p>Hold uden fravær er ikke vist</p>
   {/if}
 
-  <table class="table-zebra table shadow-xl">
-    <thead>
-      <tr>
-        <th>Hold</th>
-        <th>Fravær</th>
-      </tr>
-    </thead>
-    <tbody>
-      {#each $fravaer.generalt as fravaer}
-        {#if fravaer.hold != "Samlet" && fravaer.fravær_procent != "0,00%"}
-          <tr>
-            <td>{fravaer.hold}</td>
-            <td>{fravaer.fravær_procent}</td>
-          </tr>
-        {/if}
-      {/each}
-      <tr>
-        <td>Fag uden fravær er ikke vist</td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="mb-4 mt-4">
+    <table class="table w-full rounded-xl shadow-xl">
+      <thead>
+        <tr>
+          <th>Hold</th>
+          <th>Fravær</th>
+        </tr>
+      </thead>
+      <tbody>
+        {#each $fravaer.generalt as fravaer}
+          {#if fravaer.hold != "Samlet" && fravaer.fravær_procent != "0,00%"}
+            <tr>
+              <td>{fravaer.hold}</td>
+              <td>{fravaer.fravær_procent}</td>
+            </tr>
+          {/if}
+        {/each}
+      </tbody>
+    </table>
+  </div>
 {/if}
