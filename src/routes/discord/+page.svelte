348b2b85d5880a58813302581433fac357d97479
jona799t <jonathan.b.skettrup@gmail.com>
2022-12-05 15:24:43 +0100

    Tilføjede en /discord til at få discord serveren

---
+++ src/routes/discord/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+    window.location.href = "https://discord.gg/uj4xtSUnKN"
+</script>
+
+<p>Er du ikke blevet omdirigeret? Så klik <a class="btn btn-xs btn-primary" href="https://discord.gg/uj4xtSUnKN">her</a></p>
