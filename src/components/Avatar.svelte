abeed7b26cb3c7e90748986e5331c0ee6e3b0091
jona799t <jonathan.b.skettrup@gmail.com>
2022-12-06 11:30:55 +0100

    Nu virker /besked

--- src/components/Avatar.svelte
+++ src/components/Avatar.svelte
@@ -2,6 +2,8 @@
   export let id;
   export let navn;
 
+  export let size = "w-12";
+
   export let squared;
 
   let source;
@@ -26,19 +28,19 @@
 
 {#if source}
     <div class="avatar">
-      <div class={squared ? "w-12 rounded" : "w-12 rounded-full"}>
+      <div class={squared ? `${size} rounded` : `${size} rounded-full`}>
         <img id="${id}" src="data:image/png;base64, {source}" alt="" />
       </div>
     </div>
 {:else if navn}
   <div class="avatar placeholder">
-    <div class="bg-neutral-focus text-neutral-content rounded-full w-12 {(squared) ? "rounded" : "rounded-full"}">
+    <div class="bg-neutral-focus text-neutral-content rounded-full {size} {(squared) ? "rounded" : "rounded-full"}">
       <span class="text-xl">{navn[0]}</span>
     </div>
   </div>
 {:else}
   <div class="avatar placeholder">
-    <div class="bg-neutral-focus text-neutral-content rounded-full w-12 {(squared) ? "rounded" : "rounded-full"}">
+    <div class="bg-neutral-focus text-neutral-content rounded-full {size} {(squared) ? "rounded" : "rounded-full"}">
       <span class="text-xl">?</span>
     </div>
   </div>
