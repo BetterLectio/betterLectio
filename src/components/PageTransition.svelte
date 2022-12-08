2caeddf8a948f74547765c8165c0b337173cbda0
victor <victordj587@gmail.com>
2022-12-08 16:11:51 +0100

    Improved page transitions

--- src/components/PageTransition.svelte
+++ src/components/PageTransition.svelte
@@ -1,10 +1,11 @@
 <script>
   import { fly } from "svelte/transition";
+  import { quintOut, quintIn } from "svelte/easing";
   export let pathname = "";
 </script>
 
 {#key pathname}
-  <div in:fly={{ y: -5, duration: 200, delay: 200 }} out:fly={{ y: 5, duration: 200 }}>
+  <div in:fly={{ y: -40, duration: 300, delay: 300, easing: quintOut}} out:fly={{ y: 40, duration: 300, easing: quintIn }}>
     <slot />
   </div>
 {/key}
