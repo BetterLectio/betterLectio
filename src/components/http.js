fb0b500325edc95a7bee4f92ecfbe1b18fdfd5f3
Asguho <78410939+Asguho@users.noreply.github.com>
2022-12-13 10:39:11 +0100

    added sha256

--- src/components/http.js
+++ src/components/http.js
@@ -1,4 +1,11 @@
 import { goto } from "$app/navigation";
+
+async function sha256(str) {
+  const buf = await crypto.subtle.digest("SHA-256", new TextEncoder("utf-8").encode(str));
+  return Array.prototype.map.call(new Uint8Array(buf), (x) => ("00" + x.toString(16)).slice(-2)).join("");
+}
+const customUUID = sha256(JSON.parse(atob(localStorage.getItem("authentication"))).LastLoginUserName);
+
 export async function get(endpoint) {
   // Wait until the user is authenticated
   while (true) {
@@ -14,12 +21,14 @@ export async function get(endpoint) {
     console.log("No cookie, redirecting to auth page");
     window.location.href = "/auth";
   }
+
   // Fetch the data from the API
   const response = await fetch(`https://better-lectio-flask-backend.vercel.app${endpoint}`, {
     headers: {
       "lectio-cookie": localStorage.getItem("authentication"),
     },
   });
+
   const textResponse = await response.text();
   // If the response is ok, return the data, otherwise redirect to the auth page
   if (response.ok) {
