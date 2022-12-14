48b965b7e15911a44fb21df17b37a47ce7b3ae2e
victor <victordj587@gmail.com>
2022-12-14 20:39:26 +0100

    wrap Greeting inside function

--- src/routes/forside/+page.svelte
+++ src/routes/forside/+page.svelte
@@ -2,24 +2,28 @@
   import { brugeren } from "../../components/store.js";
   import { get } from "../../components/http.js";
 
-  let alldayGreetings = ["Velkommen tilbage", "Hejsa", "Velkommen", "Hej"];
-  let morningGreetings = ["God morgen", "Go' morgen", "Godmorgen"];
-  let afternoonGreetings = ["God eftermiddag"];
-  let eveningGreetings = ["God aften", "Tak for i dag", "Godnat"];
-  let chosenGreeting = "";
-  // Get the current time
-  let currentTime = new Date().getHours();
-  if (Math.random() > 0.5) {
-    if (currentTime >= 5 && currentTime < 12) {
-      chosenGreeting = morningGreetings[Math.floor(Math.random() * morningGreetings.length)];
-    } else if (currentTime >= 12 && currentTime < 17) {
-      chosenGreeting = afternoonGreetings[Math.floor(Math.random() * afternoonGreetings.length)];
+  function getGreeting() {
+    let alldayGreetings = ["Velkommen tilbage", "Hejsa", "Velkommen", "Hej"];
+    let morningGreetings = ["God morgen", "Go' morgen", "Godmorgen"];
+    let afternoonGreetings = ["God eftermiddag"];
+    let eveningGreetings = ["God aften", "Tak for i dag", "Godnat"];
+    let chosenGreeting = "";
+    // Get the current time
+    let currentTime = new Date().getHours();
+    if (Math.random() > 0.5) {
+      if (currentTime >= 5 && currentTime < 12) {
+        chosenGreeting = morningGreetings[Math.floor(Math.random() * morningGreetings.length)];
+      } else if (currentTime >= 12 && currentTime < 17) {
+        chosenGreeting = afternoonGreetings[Math.floor(Math.random() * afternoonGreetings.length)];
+      } else {
+        chosenGreeting = eveningGreetings[Math.floor(Math.random() * eveningGreetings.length)];
+      }
     } else {
-      chosenGreeting = eveningGreetings[Math.floor(Math.random() * eveningGreetings.length)];
+      chosenGreeting = alldayGreetings[Math.floor(Math.random() * alldayGreetings.length)];
     }
-  } else {
-    chosenGreeting = alldayGreetings[Math.floor(Math.random() * alldayGreetings.length)];
+    return chosenGreeting;
   }
+
   get("/mig").then((data) => {
     $brugeren = data;
   });
@@ -27,6 +31,6 @@
 
 <body>
   {#if $brugeren}
-    <h1 class="mb-4 text-3xl font-bold">{chosenGreeting}, {$brugeren.navn}</h1>
+    <h1 class="mb-4 text-3xl font-bold">{getGreeting()}, {$brugeren.navn}</h1>
   {/if}
 </body>
