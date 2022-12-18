e15ecf78c26710bd57d55b9d06c73a6e1b8ddb31
victor <victordj587@gmail.com>
2022-12-18 14:54:40 +0100

    update tos

--- src/routes/tos/+page.svelte
+++ src/routes/tos/+page.svelte
@@ -21,7 +21,7 @@
     <p class="my-4">
       Vi er ikke ansvarlige noget som helst, der sker med dig eller dine ting, når du bruger vores tjenester.
       Vi er ikke ansvarlige for, at du ikke kan bruge vores tjenester. Vi er ikke ansvarlige for, at
-      infomationen på vores hjemmeside er forkert. Vi er ikke ansvarlige for, noget der sker med dig, mens du
+      informationen på vores hjemmeside er forkert. Vi er ikke ansvarlige for, noget der sker med dig, mens du
       bruger vores tjenester. Vi er ikke ansvarlige for, noget der sker med dig, fordi du bruger/har brugt
       vores tjenester. Vi er ikke ansvarlige for, at du ikke kan benytte alle funktioner på vores hjemmeside.
     </p>
@@ -34,12 +34,15 @@
   </div>
   <div class="collapse-content">
     <p class="my-4">
-      Når du logger ind opbevare en Base64 formidlet/encoded/indkodet version af dit brugernavn og skoleid
-      m.m. i din browser, for at hente informationer fra Lectio, sender vi denne information til vores
-      servere, som så sender den videre til Lectio. Vi bruger ikke denne information til noget som helst, og
-      vi gemmer den ikke. (Tag et kig på vores kode for mere information) for at gøre det nemmere for dig at
-      logge ind. Vi har ikke adgang til denne information og kan ikke se den på vores servere. Vi kan dog ikke
-      garantere, at vores hostingudbydere ikke logger
+      Når du logger ind opbevare din browser en Base64 formidlet/encoded/indkodet version af dit brugernavn og
+      skoleid m.m. i din browser, for at hente informationer fra Lectio, sender vi denne information til vores
+      servere, som derefter sender den videre til Lectio. Vi bruger ikke denne information til noget som
+      helst, og vi gemmer den ikke (Tag et kig på vores kode for mere information). For at gøre det nemmere
+      for dig at logge ind. Vi har ikke adgang til denne information og kan ikke se den på vores servere. Vi
+      kan dog ikke garantere, at vores hostingudbydere ikke logger din aktivitet på vores hjemmeside. Din
+      aktivitet på https://betterlectio.dk bliver indsamles af vores analytics værktøj, som er Google
+      Analytics. i vores analytics værktøj kan vi ikke se specifikt indhold fra Lectio, men kun generelle
+      statistikker om din aktivitet på betterLectio.
     </p>
   </div>
 </div>
@@ -53,7 +56,7 @@
       Hvis du har spørgsmål til vores servicevilkår eller privatlivspolitik, så er du velkommen til at
       kontakte os på github: <a
         class="btn-primary btn-xs btn"
-        href="https://github.com/victorDigital/betterLectio/issues/new/choose">her</a
+        href="https://github.com/betterLectio/betterLectio">her</a
       >
     </p>
   </div>
