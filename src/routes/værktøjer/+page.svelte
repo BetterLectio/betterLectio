871395ba2a86125bd74dd377542b933f1c227388
victor <victordj587@gmail.com>
2022-12-19 15:59:31 +0100

    click on entire element

--- src/routes/værktøjer/+page.svelte
+++ src/routes/værktøjer/+page.svelte
@@ -1,22 +1,22 @@
 <h1 class="mb-4 text-3xl font-bold">Værktøjer</h1>
 
 <ul class="list">
-  <li class="flex element border-l-0 border-primary hover:border-l-4 transition-all duration-100">
-    <a class="flex-row " href="fri">
-      <span part="afsender" class="btn btn-xs w-fit">BETA</span>
+  <a class="flex-row " href="fri">
+    <li class="element flex border-l-0 border-primary transition-all duration-100 hover:border-l-4">
+      <span part="afsender" class="btn-xs btn w-fit mr-4">BETA</span>
       <span part="emne" class="text-lg font-bold">Hvornår har jeg fri?</span>
-    </a>
-  </li>
-  <li class="flex element border-l-0 border-primary hover:border-l-4 transition-all duration-100">
-    <a class="flex-row " href="https://www.asguho.dk/mathformatter">
-      <span part="afsender" class="btn btn-xs w-fit">BETA</span>
+    </li>
+  </a>
+  <a class="flex-row " href="https://www.asguho.dk/mathformatter">
+    <li class="element flex border-l-0 border-primary transition-all duration-100 hover:border-l-4">
+      <span part="afsender" class="btn-xs btn w-fit mr-4">BETA</span>
       <span part="emne" class="text-lg font-bold">Konverter din matematikopgave til et valgfrit format</span>
-    </a>
-  </li>
-  <li class="flex element border-l-0 border-primary hover:border-l-4 transition-all duration-100">
-    <a class="flex-row " href="/elever">
-      <span part="afsender" class="btn btn-xs w-fit">BETA</span>
+    </li>
+  </a>
+  <a class="flex-row " href="/elever">
+    <li class="element flex border-l-0 border-primary transition-all duration-100 hover:border-l-4">
+      <span part="afsender" class="btn-xs btn w-fit mr-4">BETA</span>
       <span part="emne" class="text-lg font-bold">Elev liste</span>
-    </a>
-  </li>
+    </li>
+  </a>
 </ul>
