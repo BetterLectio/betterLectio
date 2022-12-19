87163db41c65e3151400a3d756a1d9aea685ffe8
victor <victordj587@gmail.com>
2022-12-19 15:57:21 +0100

    added animations to /Værktøjer

--- src/routes/værktøjer/+page.svelte
+++ src/routes/værktøjer/+page.svelte
@@ -1,19 +1,19 @@
 <h1 class="mb-4 text-3xl font-bold">Værktøjer</h1>
 
 <ul class="list">
-  <li class="flex element">
+  <li class="flex element border-l-0 border-primary hover:border-l-4 transition-all duration-100">
     <a class="flex-row " href="fri">
       <span part="afsender" class="btn btn-xs w-fit">BETA</span>
       <span part="emne" class="text-lg font-bold">Hvornår har jeg fri?</span>
     </a>
   </li>
-  <li class="flex element">
+  <li class="flex element border-l-0 border-primary hover:border-l-4 transition-all duration-100">
     <a class="flex-row " href="https://www.asguho.dk/mathformatter">
       <span part="afsender" class="btn btn-xs w-fit">BETA</span>
       <span part="emne" class="text-lg font-bold">Konverter din matematikopgave til et valgfrit format</span>
     </a>
   </li>
-  <li class="flex element">
+  <li class="flex element border-l-0 border-primary hover:border-l-4 transition-all duration-100">
     <a class="flex-row " href="/elever">
       <span part="afsender" class="btn btn-xs w-fit">BETA</span>
       <span part="emne" class="text-lg font-bold">Elev liste</span>
