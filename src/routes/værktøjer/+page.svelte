4ae690583cd958b857e73a34fbb47588c3d63df4
Asguho <78410939+Asguho@users.noreply.github.com>
2022-12-19 13:48:11 +0100

    updated UI in /værktøjer

--- src/routes/værktøjer/+page.svelte
+++ src/routes/værktøjer/+page.svelte
@@ -1,22 +1,22 @@
 <h1 class="mb-4 text-3xl font-bold">Værktøjer</h1>
 
-<ul class="menu rounded-box w-full bg-base-100 p-2 drop-shadow-xl">
-  <li class="flex">
+<ul class="list">
+  <li class="flex element">
     <a class="flex-row " href="fri">
-      <p part="afsender" class="btn btn-xs w-fit">BETA</p>
-      <p part="emne" class="text-lg font-bold">Hvornår har jeg fri?</p>
+      <span part="afsender" class="btn btn-xs w-fit">BETA</span>
+      <span part="emne" class="text-lg font-bold">Hvornår har jeg fri?</span>
     </a>
   </li>
-  <li class="flex">
+  <li class="flex element">
     <a class="flex-row " href="https://www.asguho.dk/mathformatter">
-      <p part="afsender" class="btn btn-xs w-fit">BETA</p>
-      <p part="emne" class="text-lg font-bold">Konverter din matematikopgave til et valgfrit format</p>
+      <span part="afsender" class="btn btn-xs w-fit">BETA</span>
+      <span part="emne" class="text-lg font-bold">Konverter din matematikopgave til et valgfrit format</span>
     </a>
   </li>
-  <li class="flex">
+  <li class="flex element">
     <a class="flex-row " href="/elever">
-      <p part="afsender" class="btn btn-xs w-fit">BETA</p>
-      <p part="emne" class="text-lg font-bold">Elev liste</p>
+      <span part="afsender" class="btn btn-xs w-fit">BETA</span>
+      <span part="emne" class="text-lg font-bold">Elev liste</span>
     </a>
   </li>
 </ul>
