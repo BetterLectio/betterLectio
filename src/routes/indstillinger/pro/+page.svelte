c2dfe3d3f4a1985f6b1ac156304d05bb51d712ed
jona799t <jonathan.b.skettrup@gmail.com>
2022-12-21 16:57:16 +0100

    Stavefejl

--- src/routes/indstillinger/pro/+page.svelte
+++ src/routes/indstillinger/pro/+page.svelte
@@ -1,2 +1,2 @@
 <h1 class="mb-4 text-3xl font-bold">Indstillinger - Pro</h1>
-<p>Hvis du er contributer for du automatisk Better Lectio Pro, alt du skal gøre er at tilslutte din Github <a href="tilslut" class="btn btn-xs btn-primary">her</a>.</p>
+<p>Hvis du er contributer får du automatisk Better Lectio Pro, alt du skal gøre er at tilslutte din Github <a href="tilslut" class="btn btn-xs btn-primary">her</a>.</p>
