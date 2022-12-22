9a2c7eb09278ad4a64388232be619c9a59df5210
jona799t <jonathan.b.skettrup@gmail.com>
2022-12-22 23:09:08 +0100

    Remove console.log

--- src/routes/opgaver/+page.svelte
+++ src/routes/opgaver/+page.svelte
@@ -28,7 +28,6 @@
       let _date = opgave.frist.replace("-", "/").split(" ")
       let __date = _date[0].split("/")
       opgave.date = new Date(`${__date[1]}/${__date[0]}/${__date[2]} ${_date[1]}`);
-      console.log(opgave.date)
       if (opgave.status == "Afleveret") {
         opgave.class = "btn btn-success";
         afleveredeOpgaver.push(opgave);
