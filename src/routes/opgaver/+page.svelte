464d886fb511fb8b1694e72d3e1efdac1f0d07ac
victor <victordj587@gmail.com>
2022-12-23 20:36:48 +0100

    ran prettier

--- src/routes/opgaver/+page.svelte
+++ src/routes/opgaver/+page.svelte
@@ -6,16 +6,16 @@
   let _opgaver = [];
 
   $: opgaverIndstillinger = {
-      // dette er kun for table
-      erFristRelativITabel: false,
-      visHeleBeskrivelsenITabel: false,
-    };
+    // dette er kun for table
+    erFristRelativITabel: false,
+    visHeleBeskrivelsenITabel: false,
+  };
   if (localStorage.getItem("opgaver_indstillinger")) {
     console.log("reading from local storage");
     opgaverIndstillinger = JSON.parse(localStorage.getItem("opgaver_indstillinger"));
   } else {
     console.log("writing to local storage");
-    localStorage.setItem("opgaver_indstillinger", JSON.stringify(opgaverIndstillinger));
+    localStorage.setItem("opgaver_indstillinger", opgaverIndstillinger);
   }
 
   get("/opgaver").then((data) => {
@@ -52,8 +52,8 @@
 
     //loop trouh __opgaver
     for (const opgave of __opgaver) {
-      let _date = opgave.frist.replace("-", "/").split(" ")
-      let __date = _date[0].split("/")
+      let _date = opgave.frist.replace("-", "/").split(" ");
+      let __date = _date[0].split("/");
       opgave.date = new Date(`${__date[1]}/${__date[0]}/${__date[2]} ${_date[1]}`);
       if (opgave.status == "Afleveret") {
         opgave.class = "btn btn-success";
@@ -136,22 +136,54 @@
       bind:value={searchString}
       on:input={search}
     />
-    <div class="dropdown dropdown-bottom dropdown-end hidden ml-4 lg:inline-flex">
-      <label tabindex="0" class="btn btn-sm m-0 h-10"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16">
-        <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"/>
-        <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z"/>
-      </svg></label>
-      <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+    <div class="dropdown-end dropdown dropdown-bottom ml-4 hidden lg:inline-flex">
+      <label tabindex="0" class="btn-sm btn m-0 h-10"
+        ><svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-gear"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"
+          />
+          <path
+            d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z"
+          />
+        </svg></label
+      >
+      <ul tabindex="0" class="dropdown-content menu rounded-box w-52 bg-base-100 p-2 shadow">
         <div class="form-control">
           <label class="label cursor-pointer">
-            <span class="label-text">Er frist absolut</span> 
-            <input type="checkbox" checked="checked" class="checkbox" on:click={() => {opgaverIndstillinger.erFristRelativITabel ? opgaverIndstillinger.erFristRelativITabel = false : opgaverIndstillinger.erFristRelativITabel = true; localStorage.setItem("opgaver_indstillinger", JSON.stringify(opgaverIndstillinger));}} />
+            <span class="label-text">Er frist absolut</span>
+            <input
+              type="checkbox"
+              checked="checked"
+              class="checkbox"
+              on:click={() => {
+                opgaverIndstillinger.erFristRelativITabel
+                  ? (opgaverIndstillinger.erFristRelativITabel = false)
+                  : (opgaverIndstillinger.erFristRelativITabel = true);
+                localStorage.setItem("opgaver_indstillinger", JSON.stringify(opgaverIndstillinger));
+              }}
+            />
           </label>
         </div>
         <div class="form-control">
           <label class="label cursor-pointer">
-            <span class="label-text">Vis hele beskrivelsen</span> 
-            <input type="checkbox" checked="" class="checkbox" on:click={() => {opgaverIndstillinger.visHeleBeskrivelsenITabel ? opgaverIndstillinger.visHeleBeskrivelsenITabel = false : opgaverIndstillinger.visHeleBeskrivelsenITabel = true}} />
+            <span class="label-text">Vis hele beskrivelsen</span>
+            <input
+              type="checkbox"
+              checked=""
+              class="checkbox"
+              on:click={() => {
+                opgaverIndstillinger.visHeleBeskrivelsenITabel
+                  ? (opgaverIndstillinger.visHeleBeskrivelsenITabel = false)
+                  : (opgaverIndstillinger.visHeleBeskrivelsenITabel = true);
+              }}
+            />
           </label>
         </div>
       </ul>
@@ -178,14 +210,18 @@
                   {opgave.opgavetitel}
                 </p>
                 <p>{opgave.hold}</p>
-                <p class="line-clamp-2 {(opgave.status == "Venter") ? "mr-16" : ""}">{opgave.opgavenote}</p>
+                <p class="line-clamp-2 {opgave.status == 'Venter' ? 'mr-16' : ''}">{opgave.opgavenote}</p>
               </div>
 
               {#if opgave.status == "Venter"}
                 <div class="flex items-center">
                   <div class="absolute right-0 mr-4">
                     <p><strong>Om</strong></p>
-                    <p><strong>{dageIndtil(opgave.date)} {dageIndtil(opgave.date) != 1 ? "dage" : "dag"}</strong></p>
+                    <p>
+                      <strong
+                        >{dageIndtil(opgave.date)} {dageIndtil(opgave.date) != 1 ? "dage" : "dag"}</strong
+                      >
+                    </p>
                   </div>
                 </div>
               {/if}
@@ -215,12 +251,15 @@
               <td class="">{opgave.hold}</td>
               <td class="">
                 {#if opgaverIndstillinger.erFristRelativITabel}
-                <p class="btn-xs btn frist">{dageIndtil(opgave.date)} {dageIndtil(opgave.date) != 1 ? "dage" : "dag"}</p>
+                  <p class="frist btn-xs btn">
+                    {dageIndtil(opgave.date)}
+                    {dageIndtil(opgave.date) != 1 ? "dage" : "dag"}
+                  </p>
                 {:else}
-                <p class="btn-xs btn frist">{opgave.frist}</p>
+                  <p class="frist btn-xs btn">{opgave.frist}</p>
                 {/if}
               </td>
-              <td class="text-left beskrivelse" id={opgave.exerciseid}>
+              <td class="beskrivelse text-left" id={opgave.exerciseid}>
                 <div>
                   {opgave.opgavenote}
                 </div>
