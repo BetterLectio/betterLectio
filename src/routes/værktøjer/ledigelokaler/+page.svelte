c43a8a70363b4d2af13bd7892c95ac5edc221609
Johnny JTH <johanohly@gmail.com>
2022-12-24 21:48:44 +0000

    Fixed ledige lokaler

--- src/routes/værktøjer/ledigelokaler/+page.svelte
+++ src/routes/værktøjer/ledigelokaler/+page.svelte
@@ -3,18 +3,24 @@
   import { lokaler } from "../../../components/store.js";
 
   get("/ledige_lokaler").then((data) => {
-    $lokaler = data;
+    $lokaler = data.filter((lokale) => lokale.status == "ledigt");
   });
 </script>
 
 <ul class="list">
   <h1 class="pb-2 text-2xl font-bold">Ledige lokaler</h1>
-  {#each $lokaler as lokale}
-    {#if lokale.status == "ledigt"}
-      <li class="element flex justify-between">
-        <p class="text-lg font-bold">{lokale.lokale}</p>
-        <p class="text-sm">{lokale.status}</p>
-      </li>
+  {#if $lokaler}
+    {#if $lokaler.length == 0}
+      <p class="text-lg">Ingen ledige lokaler</p>
+    {:else}
+      {#each $lokaler as lokale}
+        <li class="element flex justify-between">
+          <p class="text-lg font-bold">{lokale.lokale}</p>
+          <p class="text-base">{lokale.status}</p>
+        </li>
+      {/each}
     {/if}
-  {/each}
+  {:else}
+    <p class="text-lg">Henter ledige lokaler...</p>
+  {/if}
 </ul>
