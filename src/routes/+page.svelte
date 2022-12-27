c1327dc16a3afcd05b6f0b4e5e8a0ceecd2a3f3e
jona799t <jonathan.b.skettrup@gmail.com>
2022-12-27 16:01:37 +0100

    Åben app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -43,7 +43,7 @@
               fill-rule="evenodd"
               d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"
             />
-          </svg>Log ind</a
+          </svg>{(localStorage.getItem("lectio-cookie")) ? "Åben app" : "Log ind"}</a
         >
         <a href="/help" class="btn-secondary btn mt-2 md:ml-4 md:mt-0" in:fly={{ duration: 1000, y: 0, x: -30, easing: quintInOut, delay: 1000 }}
           ><svg
