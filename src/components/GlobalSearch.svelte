eac2c05e9585320a2f533e2ce44d710c51c56cc1
victor digital <victordj587@gmail.com>
2022-12-29 10:25:03 +0100

    add lektier to global search

--- src/components/GlobalSearch.svelte
+++ src/components/GlobalSearch.svelte
@@ -151,12 +151,13 @@
       }
     });
 
-    // Cant test this because of i dont have any lektier :)
-    //$lektier.forEach((lektie) => {
-    //  if (lektie.opgavetitel.toLowerCase().includes(searchString.toLowerCase())) {
-    //    searchResults.lektier.push(lektie);
-    //  }
-    //});
+    $lektier.forEach((lektie) => {
+      if (lektie.aktivitet.navn.toLowerCase().includes(searchString.toLowerCase())) {
+        searchResults.lektier.push(lektie);
+      } else if (lektie.aktivitet.andet.toLowerCase().includes(searchString.toLowerCase())) {
+        searchResults.lektier.push(lektie);
+      }
+    });
 
     $forside.aktuelt.forEach((forside) => {
       if (forside.text.toLowerCase().includes(searchString.toLowerCase())) {
@@ -234,6 +235,19 @@
               <p class="hidden">{(animationDelay += i)}</p>
               <li class="w-full" in:blur={{ duration: 500, delay: animationDelay * 100 }} out:blur>
                 <a href="/opgave?exerciseid={opgave.exerciseid}">{opgave.opgavetitel}</a>
+              </li>
+            {/each}
+          {/if}
+
+          {#if searchResults.lektier.length > 0}
+            <p class="hidden">{animationDelay++}</p>
+            <li class="menu-title" in:blur={{ duration: 500, delay: animationDelay * 100 }} out:blur>
+              <span>Lektier</span>
+            </li>
+            {#each searchResults.lektier as lektie, i}
+              <p class="hidden">{(animationDelay += i)}</p>
+              <li class="w-full" in:blur={{ duration: 500, delay: animationDelay * 100 }} out:blur>
+                <a href="/modul?absid={lektie.aktivitet.absid}">{lektie.aktivitet.navn}</a>
               </li>
             {/each}
           {/if}
