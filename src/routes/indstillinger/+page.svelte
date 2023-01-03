9a4e5a88e61312a9e807fb89dea83b324e5debd7
victor digital <victordj587@gmail.com>
2023-01-03 16:58:17 +0100

    resonsive settings nav

--- src/routes/indstillinger/+page.svelte
+++ src/routes/indstillinger/+page.svelte
@@ -11,17 +11,16 @@
   let pageTransitionDirection;
 
   let prevPage = "";
-  let page = "konto"
+  let page = "konto";
 
   function changePage(newPage) {
     prevPage = page;
     page = newPage;
     determineDirection();
-    document.getElementById(prevPage).classList.remove("tab-active");
-    document.getElementById(page).classList.add("tab-active");
   }
 
-  function getPagePosition(p) { // more like getPageIndex idk
+  function getPagePosition(p) {
+    // more like getPageIndex idk
     switch (p) {
       case "konto":
         return 0;
@@ -45,34 +44,78 @@
   }
 </script>
 
-<div class="tabs tabs-boxed mb-4 w-fit">
-  <a id="konto" class="tab tab-active" on:click={() => changePage("konto")}>Konto</a>
-  <a id="api" class="tab" on:click={() => changePage("api")}>API</a>
-  <a id="pro" class="tab" on:click={() => changePage("pro")}>Pro</a>
-  <a id="tema" class="tab" on:click={() => changePage("tema")}>Tema</a>
-  <a id="tilslut" class="tab" on:click={() => changePage("tilslut")}>Tilslut</a>
+<div class="tabs tabs-boxed mb-4 w-fit md:hidden">
+  <a id="konto" class="tab {page == 'konto' ? 'tab-active' : ''}" on:click={() => changePage("konto")}
+    >Din konto</a
+  >
+  <a id="api" class="tab {page == 'api' ? 'tab-active' : ''}" on:click={() => changePage("api")}>API</a>
+  <a id="pro" class="tab {page == 'pro' ? 'tab-active' : ''}" on:click={() => changePage("pro")}>Pro</a>
+  <a id="tema" class="tab {page == 'tema' ? 'tab-active' : ''}" on:click={() => changePage("tema")}>Tema</a>
+  <a id="tilslut" class="tab {page == 'tilslut' ? 'tab-active' : ''}" on:click={() => changePage("tilslut")}
+    >Tilslut</a
+  >
 </div>
 
-<div>
-  {#if page === "konto"}
-    <div in:fly={{ x: -pageTransitionDirection, duration: 300, delay: 300, easing: quintOut}} out:fly={{ x: pageTransitionDirection, duration: 300, easing: quintIn }}>
-      <Konto />
-    </div>
-  {:else if page === "api"}
-    <div in:fly={{ x: -pageTransitionDirection, duration: 300, delay: 300, easing: quintOut}} out:fly={{ x: pageTransitionDirection, duration: 300, easing: quintIn }}>
-      <Api />
-    </div>
-  {:else if page === "pro"}
-    <div in:fly={{ x: -pageTransitionDirection, duration: 300, delay: 300, easing: quintOut}} out:fly={{ x: pageTransitionDirection, duration: 300, easing: quintIn }}>
-      <Pro />
-    </div>
-  {:else if page === "tema"}
-    <div in:fly={{ x: -pageTransitionDirection, duration: 300, delay: 300, easing: quintOut}} out:fly={{ x: pageTransitionDirection, duration: 300, easing: quintIn }}>
-      <Tema />
-    </div>
-  {:else if page === "tilslut"}
-    <div in:fly={{ x: -pageTransitionDirection, duration: 300, delay: 300, easing: quintOut}} out:fly={{ x: pageTransitionDirection, duration: 300, easing: quintIn }}>
-      <Tilslut />
-    </div>
-  {/if}
+<div class="flex min-h-screen">
+  <ul class="menu rounded-box mr-4 hidden h-fit w-52 bg-base-200 p-2 md:inline">
+    <li class="mb-1">
+      <p class="font-bold {page == 'konto' ? 'active' : ''}" on:click={() => changePage("konto")}>
+        Din konto
+      </p>
+    </li>
+    <li class="mb-1">
+      <p class="font-bold {page == 'api' ? 'active' : ''}" on:click={() => changePage("api")}>API</p>
+    </li>
+    <li class="mb-1">
+      <p class="font-bold {page == 'pro' ? 'active' : ''}" on:click={() => changePage("pro")}>
+        Better Lectio Pro
+      </p>
+    </li>
+    <li class="mb-1">
+      <p class="font-bold {page == 'tema' ? 'active' : ''}" on:click={() => changePage("tema")}>Tema</p>
+    </li>
+    <li class="mb-1">
+      <p class="font-bold {page == 'tilslut' ? 'active' : ''}" on:click={() => changePage("tilslut")}>
+        Tilsluttede tjenester
+      </p>
+    </li>
+  </ul>
+  <div>
+    {#if page === "konto"}
+      <div
+        in:fly={{ x: -pageTransitionDirection, duration: 300, delay: 300, easing: quintOut }}
+        out:fly={{ x: pageTransitionDirection, duration: 300, easing: quintIn }}
+      >
+        <Konto />
+      </div>
+    {:else if page === "api"}
+      <div
+        in:fly={{ x: -pageTransitionDirection, duration: 300, delay: 300, easing: quintOut }}
+        out:fly={{ x: pageTransitionDirection, duration: 300, easing: quintIn }}
+      >
+        <Api />
+      </div>
+    {:else if page === "pro"}
+      <div
+        in:fly={{ x: -pageTransitionDirection, duration: 300, delay: 300, easing: quintOut }}
+        out:fly={{ x: pageTransitionDirection, duration: 300, easing: quintIn }}
+      >
+        <Pro />
+      </div>
+    {:else if page === "tema"}
+      <div
+        in:fly={{ x: -pageTransitionDirection, duration: 300, delay: 300, easing: quintOut }}
+        out:fly={{ x: pageTransitionDirection, duration: 300, easing: quintIn }}
+      >
+        <Tema />
+      </div>
+    {:else if page === "tilslut"}
+      <div
+        in:fly={{ x: -pageTransitionDirection, duration: 300, delay: 300, easing: quintOut }}
+        out:fly={{ x: pageTransitionDirection, duration: 300, easing: quintIn }}
+      >
+        <Tilslut />
+      </div>
+    {/if}
+  </div>
 </div>
