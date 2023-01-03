e1ac1b778d21c33316f45736c24369ef20df4687
Asguho <78410939+Asguho@users.noreply.github.com>
2023-01-03 16:50:28 +0100

    added autologin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,9 @@
   import { fly } from "svelte/transition";
   import { quintIn, quintInOut, quintOut } from "svelte/easing";
 
-  //if (localStorage.getItem("lectio-cookie")) {
-  //  window.location.href = "/forside";
-  //}
+  if (localStorage.getItem("lectio-cookie")) {
+    window.location.href = "/forside";
+  }
 </script>
 
 <div class="flex flex-row xl:justify-around">
@@ -26,7 +26,10 @@
         Prøv BetterLectio i dag og oplev, hvordan det kan forbedre din Lectio-oplevelse
       </p>
       <div class="mt-4 flex flex-col md:flex-row">
-        <a href={(localStorage.getItem("lectio-cookie")) ? "/forside" : "/auth"} class="btn-primary btn" in:fly={{ duration: 1000, y: 0, x: -30, easing: quintInOut, delay: 700 }}
+        <a
+          href={localStorage.getItem("lectio-cookie") ? "/forside" : "/auth"}
+          class="btn-primary btn"
+          in:fly={{ duration: 1000, y: 0, x: -30, easing: quintInOut, delay: 700 }}
           ><svg
             xmlns="http://www.w3.org/2000/svg"
             width="16"
@@ -43,7 +46,7 @@
               fill-rule="evenodd"
               d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"
             />
-          </svg>{(localStorage.getItem("lectio-cookie")) ? "Åben app" : "Log ind"}</a
+          </svg>{localStorage.getItem("lectio-cookie") ? "Åben app" : "Log ind"}</a
         >
         <a
           href="/help"
@@ -65,7 +68,11 @@
             />
           </svg>Hvordan virker det?</a
         >
-        <a href="https://github.com/BetterLectio/betterLectio" target="_blank" class="btn-secondary btn mt-2 md:ml-4 md:mt-0" in:fly={{ duration: 1000, y: 0, x: -30, easing: quintInOut, delay: 1300 }}
+        <a
+          href="https://github.com/BetterLectio/betterLectio"
+          target="_blank"
+          class="btn-secondary btn mt-2 md:ml-4 md:mt-0"
+          in:fly={{ duration: 1000, y: 0, x: -30, easing: quintInOut, delay: 1300 }}
           ><svg
             xmlns="http://www.w3.org/2000/svg"
             width="16"
@@ -86,7 +93,7 @@
     version="1.1"
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 512 512"
-    class="mx-0 hidden rotate-12 fill-current p-0 xl:flex h-80 w-80"
+    class="mx-0 hidden h-80 w-80 rotate-12 fill-current p-0 xl:flex"
   >
     <path
       d="M428.033,162.858C422.975,72.232,347.87,0,256,0S89.025,72.232,83.967,162.858c-0.323,3.179-0.489,6.402-0.489,9.663c0,15.543,0,320.162,0,322.783c0,9.22,7.475,16.696,16.696,16.696h111.304c119.87,0,217.043-97.174,217.043-217.043V172.522C428.522,169.261,428.356,166.037,428.033,162.858z M256,33.391c42.163,0,79.93,18.923,105.464,48.641c-8.722-2.662-17.967-4.119-27.551-4.119c-32.296,0-60.834,16.291-77.913,41.06c-17.079-24.77-45.617-41.06-77.913-41.06c-9.584,0-18.829,1.457-27.551,4.119C176.07,52.314,213.837,33.391,256,33.391z M163.169,360.642c-5.842-7.12-16.37-8.174-23.494-2.326c-6.988,5.731-14.635,10.267-22.804,13.595v-97.801c26.505,36.201,66.686,61.636,112.937,68.728c-18.13,49.489-60.64,87.32-112.937,98.783v-34.418h-0.001c16.082-4.569,30.884-12.325,43.979-23.061C167.978,378.293,169.017,367.772,163.169,360.642z M395.13,294.957c0,101.261-82.386,183.652-183.652,183.652H116.87v-2.97c70.214-12.524,126.789-63.872,147.277-131.009c53.767-2.53,101.081-29.682,130.984-70.521V294.957z M256,311.652c-76.718,0-139.13-62.413-139.13-139.13c0-33.75,27.462-61.217,61.217-61.217s61.217,27.468,61.217,61.217c0,9.22,7.475,16.696,16.696,16.696s16.696-7.475,16.696-16.696c0-33.75,27.462-61.217,61.217-61.217c33.755,0,61.217,27.468,61.217,61.217C395.13,249.239,332.718,311.652,256,311.652z"
@@ -105,7 +112,9 @@
 
 <div class="mt-6 rounded-lg bg-base-200 p-4">
   <h1 class="mb-2 text-4xl font-extrabold">Hvad er BetterLectio?</h1>
-  <p class="w-fit h-fit font-bold">BetterLectio er på nuværende tidspunkt under udvikling, det betyder at der kan forekomme fejl og mangler.</p>
+  <p class="h-fit w-fit font-bold">
+    BetterLectio er på nuværende tidspunkt under udvikling, det betyder at der kan forekomme fejl og mangler.
+  </p>
 
   <p>
     BetterLectio er en forbredelse af Lectio. Lectio er et dansk webbaseret lektionssystem udviklet af det
