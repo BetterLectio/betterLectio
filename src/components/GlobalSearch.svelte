20deb10e38ea4a5211771f496f45de4433373fc5
victor digital <victordj587@gmail.com>
2023-01-07 21:36:13 +0100

    fix to global search

--- src/components/GlobalSearch.svelte
+++ src/components/GlobalSearch.svelte
@@ -234,7 +234,7 @@
             {#each searchResults.opgaver as opgave, i}
               <p class="hidden">{(animationDelay += i)}</p>
               <li class="w-full" in:blur={{ duration: 500, delay: animationDelay * 100 }} out:blur>
-                <a href="/opgave?exerciseid={opgave.exerciseid}">{opgave.opgavetitel}</a>
+                <a href="/opgave?exerciseid={opgave.exerciseid}" rel="external">{opgave.opgavetitel}</a>
               </li>
             {/each}
           {/if}
@@ -247,7 +247,7 @@
             {#each searchResults.lektier as lektie, i}
               <p class="hidden">{(animationDelay += i)}</p>
               <li class="w-full" in:blur={{ duration: 500, delay: animationDelay * 100 }} out:blur>
-                <a href="/modul?absid={lektie.aktivitet.absid}">{lektie.aktivitet.navn || lektie.aktivitet.hold}</a>
+                <a href="/modul?absid={lektie.aktivitet.absid}" rel="external">{lektie.aktivitet.navn || lektie.aktivitet.hold}</a>
               </li>
             {/each}
           {/if}
@@ -260,7 +260,7 @@
             {#each searchResults.forside as forside, i}
               <p class="hidden">{(animationDelay += i)}</p>
               <li class="w-full" in:blur={{ duration: 500, delay: animationDelay * 100 }} out:blur>
-                <a href="/forside" class="w-full overflow-x-scroll">{forside.text}</a>
+                <a href="/forside" class="w-full overflow-x-scroll" rel="external">{forside.text}</a>
               </li>
             {/each}
           {/if}
@@ -273,7 +273,7 @@
             {#each searchResults.skema as modul, i}
               <p class="hidden">{(animationDelay += i)}</p>
               <li class="w-full" in:blur={{ duration: 500, delay: animationDelay * 100 }} out:blur>
-                <a href="/modul?absid={modul.absid}">
+                <a href="/modul?absid={modul.absid}" rel="external">
                   {#if modul.navn}
                     {modul.navn}
                   {:else if modul.hold}
@@ -294,7 +294,7 @@
             {#each searchResults.beskeder as besked, i}
               <p class="hidden">{(animationDelay += i)}</p>
               <li class="w-full" in:blur={{ duration: 500, delay: animationDelay * 100 }} out:blur>
-                <a href="/besked?id={besked.message_id}">{besked.emne}</a>
+                <a href="/besked?id={besked.message_id}" rel="external">{besked.emne}</a>
               </li>
             {/each}
           {/if}
