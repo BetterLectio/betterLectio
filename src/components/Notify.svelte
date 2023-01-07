c3f30dd31099579891338239ed96f2a16796cd12
victor digital <victordj587@gmail.com>
2023-01-07 13:09:31 +0100

    new notification animations

--- src/components/Notify.svelte
+++ src/components/Notify.svelte
@@ -1,11 +1,11 @@
 <script>
-  import { fade } from "svelte/transition";
+  import { blur } from "svelte/transition";
   import { notifications } from "./notifyStore.js";
 </script>
 
 <div class="toast toast-bottom toast-start z-10 inline h-fit">
   {#each $notifications as toast}
-  <div class="alert h-fit shadow-2xl mb-4 {toast.bgColor}" out:fade>
+  <div class="alert h-fit shadow-2xl mb-4 {toast.bgColor}" out:blur>
     <div class="h-fit">
       <span class="h-fit w-56">
         {toast.message}
