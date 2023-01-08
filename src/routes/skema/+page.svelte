2dcc4be861c72d240b7114b314bdcb9e5fe1b7b4
victor digital <victordj587@gmail.com>
2023-01-08 14:35:11 +0100

    init

--- src/routes/skema/+page.svelte
+++ src/routes/skema/+page.svelte
@@ -2,14 +2,47 @@
   import { onMount } from "svelte";
   import { get } from "../../components/http";
   import { skema } from "../../components/store";
-  import Calendar from "@event-calendar/core"; 
-  import timeGrid from "@event-calendar/time-grid";
+  import Calendar from "@event-calendar/core";
+  import TimeGrid from "@event-calendar/time-grid";
+  import DayGrid from "@event-calendar/day-grid";
+  import List from "@event-calendar/list";
   import { addNotification } from "../../components/notifyStore";
   addNotification("Skema siden er stadig under udvikling", "alert-warning");
+
+  let ec; // Event Calendar
+
+  let plugins = [TimeGrid, DayGrid, List];
+  let options = {
+    locale: new Intl.DateTimeFormat("da-DK").resolvedOptions().locale,
+    view: "timeGridWeek",
+    events: [],
+    allDaySlot: false,
+    nowIndicator: true,
+    firstDay: 1,
+    
+  };
+
+  function getWeekNumber() {
+    var d = new Date(Date.now());
+    d.setHours(0, 0, 0, 0);
+    d.setDate(d.getDate() + 4 - (d.getDay() || 7));
+    var yearStart = new Date(d.getFullYear(), 0, 1);
+    var weekNo = Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
+    return weekNo;
+  }
+
+  function getSkema() {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const currentWeekNr = getWeekNumber();
+    console.log(currentYear, currentWeekNr);
+  }
+  
+  onMount(() => {
+    getSkema();
+  });
 </script>
 
-<div class="bg-gray-900 rounded-lg p-4">
-  <Calendar/>
+<div class="rounded-lg bg-gray-100 p-4">
+  <Calendar bind:this={ec} {plugins} {options} />
 </div>
-
-
