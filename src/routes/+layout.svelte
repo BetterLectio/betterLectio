7d84159fb3c2c9d34ccec508c0cb44913a41c836
victor digital <victordj587@gmail.com>
2023-01-09 21:10:32 +0100

    one button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -84,8 +84,34 @@
 
 <div class="drawer h-screen">
   <input id="menu-drawer" type="checkbox" class="drawer-toggle" bind:checked />
+
+  <div class="bg-base-300 h-100v w-16 z-30 inline lg:absolute p-1">
+    <a href="/forside" class="group btn p-0 w-full mb-1 transition-[width] duration-500 hover:w-44 h-14 flex flex-row overflow-x-hidden">
+      <svg
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            height="40"
+            class="mx-0 fill-current p-0"
+          >
+            <path
+              d="M428.033,162.858C422.975,72.232,347.87,0,256,0S89.025,72.232,83.967,162.858c-0.323,3.179-0.489,6.402-0.489,9.663c0,15.543,0,320.162,0,322.783c0,9.22,7.475,16.696,16.696,16.696h111.304c119.87,0,217.043-97.174,217.043-217.043V172.522C428.522,169.261,428.356,166.037,428.033,162.858z M256,33.391c42.163,0,79.93,18.923,105.464,48.641c-8.722-2.662-17.967-4.119-27.551-4.119c-32.296,0-60.834,16.291-77.913,41.06c-17.079-24.77-45.617-41.06-77.913-41.06c-9.584,0-18.829,1.457-27.551,4.119C176.07,52.314,213.837,33.391,256,33.391z M163.169,360.642c-5.842-7.12-16.37-8.174-23.494-2.326c-6.988,5.731-14.635,10.267-22.804,13.595v-97.801c26.505,36.201,66.686,61.636,112.937,68.728c-18.13,49.489-60.64,87.32-112.937,98.783v-34.418h-0.001c16.082-4.569,30.884-12.325,43.979-23.061C167.978,378.293,169.017,367.772,163.169,360.642z M395.13,294.957c0,101.261-82.386,183.652-183.652,183.652H116.87v-2.97c70.214-12.524,126.789-63.872,147.277-131.009c53.767-2.53,101.081-29.682,130.984-70.521V294.957z M256,311.652c-76.718,0-139.13-62.413-139.13-139.13c0-33.75,27.462-61.217,61.217-61.217s61.217,27.468,61.217,61.217c0,9.22,7.475,16.696,16.696,16.696s16.696-7.475,16.696-16.696c0-33.75,27.462-61.217,61.217-61.217c33.755,0,61.217,27.468,61.217,61.217C395.13,249.239,332.718,311.652,256,311.652z"
+            />
+            <path
+              d="M301.195,216.369c-3.26-3.26-7.532-4.891-11.804-4.891c-4.272,0-8.544,1.631-11.804,4.892L256,237.956l-21.587-21.588c-6.521-6.521-17.087-6.521-23.609,0c-6.521,6.521-6.521,17.087,0,23.609l33.391,33.391c6.521,6.521,17.087,6.521,23.609,0l33.391-33.391C307.716,233.456,307.716,222.89,301.195,216.369z"
+            />
+            <path
+              d="M178.087,150.261c-12.277,0-22.261,9.99-22.261,22.261s9.984,22.261,22.261,22.261s22.261-9.99,22.261-22.261 S190.364,150.261,178.087,150.261z"
+            />
+            <path
+              d="M333.913,150.261c-12.277,0-22.261,9.99-22.261,22.261s9.984,22.261,22.261,22.261c12.277,0,22.261-9.99,22.261-22.261 S346.19,150.261,333.913,150.261z"
+            />
+          </svg><span class=" opacity-0 w-0 group-hover:w-20 group-hover:opacity-100 transition-all duration-500">Forside</span>
+    </a>
+  </div>
+
   <div class="drawer-content">
-    <div class="navbar relative z-50 mb-10">
+    <div class="navbar relative z-50 mb-10 inline-flex lg:hidden">
       <div class="navbar-start">
         <p />
         <div class="dropdown">
@@ -141,24 +167,6 @@
             />
           </svg><span class="hidden md:block">Better Lectio</span>
         </p>
-      </div>
-      <div class="navbar-center hidden xl:flex">
-        <ul class="menu menu-horizontal p-0">
-          {#if $brugeren && localStorage.getItem("lectio-cookie")}
-            <li><a href="/skema">Skema</a></li>
-            <li><a href="/opgaver">Opgaver</a></li>
-            <li><a href="/lektier">Lektier</a></li>
-            <li><a href="/fravær">Fravær</a></li>
-            <li><a href="/dokumenter">Dokumenter</a></li>
-            <li><a href="/beskeder">Beskeder</a></li>
-            <li><a href="/karakterer">Karakterer</a></li>
-            <li><a href="/værktøjer">Værktøjer</a></li>
-          {:else}
-            <li><a href="/">Forside</a></li>
-            <li><a href="/help">Hjælp</a></li>
-            <li><a href="/tos">Servicevilkår & Privatlivspolitik</a></li>
-          {/if}
-        </ul>
       </div>
       <div class="navbar-center flex sm:hidden">
         <p
@@ -324,13 +332,6 @@
       <PageTransition pathname={data.pathname}>
         <slot />
       </PageTransition>
-    </div>
-
-    <!-- Her vil notificationer/alerts blive tilføjet-->
-    <div class="fixed bottom-10 right-10 left-10">
-      <div class="flex items-center justify-center">
-        <div id="alerts" />
-      </div>
     </div>
   </div>
   <div class="drawer-side">
@@ -565,3 +566,21 @@
     </ul>
   </div>
 </div>
+
+
+<!--
+{#if $brugeren && localStorage.getItem("lectio-cookie")}
+  <li><a href="/skema">Skema</a></li>
+  <li><a href="/opgaver">Opgaver</a></li>
+  <li><a href="/lektier">Lektier</a></li>
+  <li><a href="/fravær">Fravær</a></li>
+  <li><a href="/dokumenter">Dokumenter</a></li>
+  <li><a href="/beskeder">Beskeder</a></li>
+  <li><a href="/karakterer">Karakterer</a></li>
+  <li><a href="/værktøjer">Værktøjer</a></li>
+{:else}
+  <li><a href="/">Forside</a></li>
+  <li><a href="/help">Hjælp</a></li>
+  <li><a href="/tos">Servicevilkår & Privatlivspolitik</a></li>
+{/if}
+-->
