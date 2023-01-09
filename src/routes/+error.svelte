9b8469a52daceafaa06a0a503163ae16640cc5f9
victor digital <victordj587@gmail.com>
2023-01-09 19:49:01 +0100

    better 404

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,18 +1,23 @@
 <script>
-  let fejlBeskrivelser = [
-    "Denne side eksisterer ikke. Prøv at gå tilbage til forsiden.",
-    "Vi kunne ikke finde siden du leder efter",
-    "Vi gjorer vores bedste, men vi kunne ikke finde siden du leder efter",
-    "Vi har ikke fundet siden du leder efter",
-    "Vi unskylder men vi kunne ikke finde siden du leder efter",
-  ];
-  let fejlBeskrivelse = fejlBeskrivelser[Math.floor(Math.random() * fejlBeskrivelser.length)];
+  import { blur } from 'svelte/transition';
+  import { addNotification } from '../components/notifyStore';
+
+  const getCurrentEndpoint = () => {
+    const url = new URL(window.location.href);
+    return url.pathname;
+  };
+    addNotification(`404 - kunne ikke finde ${getCurrentEndpoint()}`, 'alert-error');
 </script>
 
-<div
-  class="loader absolute top-1/2 left-1/2 flex -translate-x-1/2 -translate-y-1/2 transform flex-col items-center"
->
-  <span class="my-4 text-3xl">404</span>
-  <span class="my-4 text-2xl font-bold">{fejlBeskrivelse}</span>
-  <a href="/" class="btn-accent btn my-4">Tilbage</a>
-</div>
+
+<h1 class="text-9xl font-black text-center" in:blur="{{duration: 1300}}">
+  404
+</h1>
+
+<h2 class="text-3xl font-black text-center" in:blur="{{duration: 1300, delay: 325}}">
+  Det lader til at du er gået lidt forkert...
+</h2>
+
+<p class="text-center mt-10" in:blur="{{duration: 1300, delay: 650}}">
+  <a href="/forside" class="btn btn-sm">Gå tilbage</a>
+</p>
