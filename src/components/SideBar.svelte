ff44eafa1652dd042c08880ea12706ccc7c77cb6
victor digital <victordj587@gmail.com>
2023-01-10 19:23:41 +0100

    centered some buttons in sidebar

--- src/components/SideBar.svelte
+++ src/components/SideBar.svelte
@@ -125,7 +125,7 @@
   </div>
   <div>
     <div class="dropdown dropdown-top z-50 w-full mb-1">
-      <button class="btn btn-sm normal-case w-full" for="theme-select">
+      <button class="btn btn-sm w-full" for="theme-select">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="25"
@@ -149,7 +149,7 @@
     {#if $brugeren && localStorage.getItem("lectio-cookie") && cookie}
     <a
       href="/indstillinger"
-      class="group btn mb-1 flex h-14 w-full flex-row p-0"
+      class="group btn h-14 w-full mb-1"
     >
     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
       <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
