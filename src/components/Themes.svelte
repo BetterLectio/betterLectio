fb60da705b6162332a7e53314e4ced1543f04058
victor <victordj587@gmail.com>
2023-01-12 13:29:30 +0100

    nyt tema

--- src/components/Themes.svelte
+++ src/components/Themes.svelte
@@ -159,3 +159,26 @@
     </div>
   </button>
 </li>
+<li>
+  <button class="mx-0 my-1 p-0 hover:scale-110" data-set-theme="coffee" data-act-class="ACTIVECLASS">
+    <div
+      class="w-full overflow-hidden rounded-lg text-left outline outline-2 outline-offset-2 outline-base-content"
+      data-set-theme="coffee"
+      data-act-class="outline"
+    >
+      <div data-theme="coffee" class="w-full cursor-pointer bg-base-100 font-sans text-base-content">
+        <div class="grid grid-cols-5 grid-rows-3">
+          <div class="col-span-5 row-span-3 row-start-1 flex gap-1 py-3 px-4">
+            <div class="flex-grow text-sm font-bold">Kaffe</div>
+            <div class="flex flex-shrink-0 flex-wrap gap-1">
+              <div class="w-2 rounded bg-primary" />
+              <div class="w-2 rounded bg-secondary" />
+              <div class="w-2 rounded bg-accent" />
+              <div class="w-2 rounded bg-neutral" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </button>
+</li>
