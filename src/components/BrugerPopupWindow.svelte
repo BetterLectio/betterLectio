7eb7b298f624f91660c42e58690dd9c54a659636
Asguho <78410939+Asguho@users.noreply.github.com>
2023-01-13 15:55:00 +0100

    Update BrugerPopupWindow.svelte

--- src/components/BrugerPopupWindow.svelte
+++ src/components/BrugerPopupWindow.svelte
@@ -1,20 +1,42 @@
 <script>
+  import { get } from "./http.js";
   import Avatar from "./Avatar.svelte";
 
   export let navn;
   export let id;
+
+  let brugerData = null;
+  get("/skema?id=" + id).then((data) => {
+    brugerData = data;
+  });
+  function getGrupperAsString(grupper) {
+    let gruppeNavne = [];
+    grupper.forEach((gruppe) => {
+      gruppeNavne.push(gruppe.navn);
+    });
+    return gruppeNavne.join(", ");
+  }
 </script>
 
 <div class="card w-96 shadow-2xl">
   <div class="card-body">
     <Avatar {id} {navn} size="w-24" clickable={false} />
     <h2 class="card-title">{navn}</h2>
-    <p>Dataen herunder er et eksempel på hvordan brugerdropdown kan se ud. Dataen er altså ikke korekt.</p>
-    <p><strong>Hold & grupper:</strong> {"Alle 1. HTX-elever (Lyn), Programmeringsklubben"}</p>
-    <p><strong>Aktivitet:</strong> {["Matematik i lokale 2.04", "Fri"][Math.floor(Math.random() * 2)]}</p>
-    <div class="card-actions mt-4 justify-start">
-      <button class="btn-primary btn">Se skema</button>
-      <button class="btn-primary btn">Send besked</button>
-    </div>
+    {#if brugerData !== null}
+      {#if brugerData?.grupper}
+        <p>
+          <strong>Hold & grupper:</strong>
+          {getGrupperAsString(brugerData?.grupper)}
+        </p>
+      {/if}
+
+      <p><strong>Aktivitet:</strong> {brugerData?.aktivitet}</p>
+      <div class="card-actions mt-4 justify-start">
+        <button class="btn-primary btn">Se skema</button>
+        <button class="btn-primary btn">Send besked</button>
+      </div>
+    {:else}
+      <div class="text-center">Henter brugerData...</div>
+    {/if}
   </div>
 </div>
