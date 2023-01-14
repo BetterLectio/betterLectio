c2024852246949733dfb1a82d8cb22b8f310d2b5
jona799t <jonathan.b.skettrup@gmail.com>
2023-01-14 12:14:21 +0100

    Grupper og hold virker nu

--- src/components/BrugerPopupWindow.svelte
+++ src/components/BrugerPopupWindow.svelte
@@ -1,20 +1,41 @@
 <script>
   import Avatar from "./Avatar.svelte";
+  import { get } from "./http.js";
 
   export let navn;
   export let id;
+
+  let skema;
+  get(`/skema?id=${id}`).then((data) => {
+    skema = data;
+  });
 </script>
 
-<div class="card w-96 shadow-2xl">
-  <div class="card-body">
-    <Avatar {id} {navn} size="w-24" clickable={false} />
-    <h2 class="card-title">{navn}</h2>
-    <p>Dataen herunder er et eksempel på hvordan brugerdropdown kan se ud. Dataen er altså ikke korekt.</p>
-    <p><strong>Hold & grupper:</strong> {"Alle 1. HTX-elever (Lyn), Programmeringsklubben"}</p>
-    <p><strong>Aktivitet:</strong> {["Matematik i lokale 2.04", "Fri"][Math.floor(Math.random() * 2)]}</p>
-    <div class="card-actions mt-4 justify-start">
-      <button class="btn-primary btn">Se skema</button>
-      <button class="btn-primary btn">Send besked</button>
+{#if skema}
+  <div class="card w-96 shadow-2xl">
+    <div class="card-body">
+      <Avatar {id} {navn} size="w-24" clickable={false} />
+      <h2 class="card-title">{navn}</h2>
+      <p>Dataen herunder er et eksempel på hvordan brugerdropdown kan se ud. Dataen er altså ikke korekt.</p>
+      {#if skema["type"] == "elev"}
+        <p><strong>Grupper:</strong>
+          {#each skema["grupper"] as gruppe}
+            <a href="/skema?id=HE{gruppe["id"]}" class="btn btn-xs btn-primary mr-1">{gruppe["navn"]}</a>
+          {/each}
+        </p>
+      {:else if skema["type"] == "lærer"}
+      <p><strong>Hold:</strong>
+        {#each Object.entries(skema["hold"]) as hold}
+          <a href="/skema?id=HE{hold[1]}" class="btn btn-xs btn-primary mr-1">{hold[0]}</a>
+        {/each}
+      </p>
+        
+      {/if}
+      <p><strong>Aktivitet:</strong> {["Matematik i lokale 2.04", "Fri"][Math.floor(Math.random() * 2)]}</p>
+      <div class="card-actions mt-4 justify-start">
+        <button class="btn-primary btn">Se skema</button>
+        <button class="btn-primary btn">Send besked</button>
+      </div>
     </div>
   </div>
-</div>
+{/if}
---
+++ src/routes/værktøjer/eksamensberegner/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { get } from "../../../components/http.js";
+    import { studieretningspraesentation } from "../../../components/store.js";
+  
+    get("/studieretningspraesentation").then((data) => {
+      $studieretningspraesentation = data;
+    });
+</script>
+
+{#if $studieretningspraesentation}
+    <p>{JSON.stringify($studieretningspraesentation)}</p>
+{/if}
