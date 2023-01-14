460b2247170532d5586a87e99b79fe7b3381f09d
victor digital <victordj587@gmail.com>
2023-01-14 13:23:28 +0100

    added warning

--- src/routes/indstillinger/routes/Layout.svelte
+++ src/routes/indstillinger/routes/Layout.svelte
@@ -1,5 +1,13 @@
 <script>
   import { instillinger } from "../../../components/store";
+
+  let windowWidth = window.innerWidth;
+
+  function handleResize() {
+    windowWidth = window.innerWidth;
+  }
+
+  window.addEventListener("resize", handleResize);
 </script>
 
 <h1 class="mb-4 text-3xl font-bold">Indstillinger - Layout</h1>
@@ -9,8 +17,12 @@
     <p>Skift imellem sidebar og topbar layout på store skærme</p>
     <label class="label cursor-pointer">
       <span class="label-text">Sidebar</span> 
-      <input type="checkbox" class="checkbox" bind:checked={$instillinger.sidebar}/>
+      {#if windowWidth > 768}
+      <input type="checkbox" class="checkbox " bind:checked={$instillinger.sidebar}/>
+      {:else}
+      <input type="checkbox" class="checkbox " bind:checked={$instillinger.sidebar} disabled/>
+      {/if}
     </label>
+    <p class="text-error ">{windowWidth < 768 ? "Ikke understøttet på din enhed" : ""}</p>
   </div>
-
 </div>
