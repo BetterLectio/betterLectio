d16de2a4ab6421db745c60c372161f948a4c0ca8
Asguho <78410939+Asguho@users.noreply.github.com>
2023-01-20 16:18:39 +0100

    fixed bug

--- src/components/BrugerPopupWindow.svelte
+++ src/components/BrugerPopupWindow.svelte
@@ -11,7 +11,7 @@
   get(`/skema?id=${id}`).then((data) => {
     skema = data;
 
-    skema["moduler"].forEach(modul => {
+    skema["moduler"].forEach((modul) => {
       let start = modul["tidspunkt"].split(" til ")[0];
       start = {
         dag: start.split("/")[0].length == 1 ? "0" + start.split("/")[0] : start.split("/")[0],
@@ -35,8 +35,8 @@
         tidspunkt: slut.split(" ")[1],
       };
 
-      modul.start = new Date(`${start.år}-${start.måned}-${start.dag}T${start.tidspunkt}`)
-      modul.slut = new Date(`${slut.år}-${slut.måned}-${slut.dag}T${slut.tidspunkt}`)
+      modul.start = new Date(`${start.år}-${start.måned}-${start.dag}T${start.tidspunkt}`);
+      modul.slut = new Date(`${slut.år}-${slut.måned}-${slut.dag}T${slut.tidspunkt}`);
 
       let now = new Date(Date.now());
 
@@ -53,20 +53,21 @@
       <Avatar {id} {navn} size="w-24" clickable={false} />
       <h2 class="card-title">{navn}</h2>
       {#if skema["type"] == "elev"}
-        <p><strong>Grupper:</strong>
+        <p>
+          <strong>Grupper:</strong>
           {#each skema["grupper"] as gruppe}
-            <a href="/skema?id=HE{gruppe["id"]}" class="btn btn-xs btn-primary mr-1">{gruppe["navn"]}</a>
+            <a href="/skema?id=HE{gruppe['id']}" class="btn-primary btn-xs btn mr-1">{gruppe["navn"]}</a>
           {/each}
         </p>
       {:else if skema["type"] == "lærer"}
-      <p><strong>Hold:</strong>
-        {#each Object.entries(skema["hold"]) as hold}
-          <a href="/skema?id=HE{hold[1]}" class="btn btn-xs btn-primary mr-1">{hold[0]}</a>
-        {/each}
-      </p>
-        
+        <p>
+          <strong>Hold:</strong>
+          {#each Object.entries(skema["hold"]) as hold}
+            <a href="/skema?id=HE{hold[1]}" class="btn-primary btn-xs btn mr-1">{hold[0]}</a>
+          {/each}
+        </p>
       {/if}
-      <p><strong>Aktivitet:</strong> {(aktivitet == undefined) ? "Fri" : `${aktivitet} i lokale ${lokale}`}</p>
+      <p><strong>Aktivitet:</strong> {aktivitet == undefined ? "Fri" : `${aktivitet} i lokale ${aktivitet.lokale}`}</p>
       <div class="card-actions mt-4 justify-start">
         <button class="btn-primary btn">Se skema</button>
         <button class="btn-primary btn">Send besked</button>
