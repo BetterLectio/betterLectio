8abbfc61ff1b7c00ed4c1f0c620a9cefe57f152d
jona799t <jonathan.b.skettrup@gmail.com>
2023-01-21 13:17:35 +0100

    virker i dev

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,16 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false,
+      strict: false
+    }),
   },
   preprocess: [
     preprocess({
