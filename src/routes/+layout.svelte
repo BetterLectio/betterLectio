76a3570a4008b359c885a34f2929be473e2870c5
victor digital <victordj587@gmail.com>
2023-01-28 13:11:01 +0100

    not allow

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -22,8 +22,7 @@
     themeChange(false);
     if (
       window.location.href.indexOf("betlec.netlify.app") > -1 &&
-      window.location.href.indexOf("dev--betlec.netlify.app") == -1 &&
-      window.location.href.indexOf("tema-fix--betlec.netlify.app") == -1
+      window.location.href.indexOf("dev--betlec.netlify.app") == -1
     ) {
       window.location.href = "https://betterlectio.dk";
     }
