a9a1eb96d5b0e3182339b2bfc6cfd853c88e2cc4
Asguho <78410939+Asguho@users.noreply.github.com>
2023-01-29 17:39:26 +0100

    Update /værktøjer/fravær

--- src/routes/værktøjer/fravær/+page.svelte
+++ src/routes/værktøjer/fravær/+page.svelte
@@ -4,68 +4,76 @@
   import { fravaer } from "../../../components/store";
 
   let desiredAmount = 10;
-  let heleAaret = {};
-  let opgjort = {};
+  let fravaerData = {};
+  let fravaerType = "opgjort"
   get("/fravaer").then((data) => {
     $fravaer = data;
+    getData()
   });
 
-  $: $fravaer?.generalt.forEach((element) => {
+  getData()
+  function getData(){
+    $fravaer?.generalt.forEach((element) => {
     if (element.hold == "Samlet") {
-      heleAaret.matches = /(\d+\,?\d*|\,\d+)\/(\d+\,?\d*|\,\d+)/g.exec(element.heleåret_fravær_moduler);
-      heleAaret.currentClasses = Number(heleAaret.matches[1].replace(",", "."));
-      heleAaret.totalClasses = Number(heleAaret.matches[2]);
-      heleAaret.currentFravær = (heleAaret.currentClasses / heleAaret.totalClasses) * 100;
-      heleAaret.possibleAmount = Math.floor(heleAaret.totalClasses * (desiredAmount / 100));
-
-      opgjort.matches = /(\d+\,?\d*|\,\d+)\/(\d+\,?\d*|\,\d+)/g.exec(element.opgjort_fravær_moduler);
-      opgjort.currentClasses = Number(opgjort.matches[1].replace(",", "."));
-      opgjort.totalClasses = Number(opgjort.matches[2]);
-      opgjort.currentFravær = (opgjort.currentClasses / opgjort.totalClasses) * 100;
-      opgjort.possibleAmount = Math.floor(opgjort.totalClasses * (desiredAmount / 100));
+      fravaerData.matches = /(\d+\,?\d*|\,\d+)\/(\d+\,?\d*|\,\d+)/g.exec(element[fravaerType+"_fravær_moduler"]);
+      fravaerData.currentClasses = Number(fravaerData.matches[1].replace(",", "."));
+      fravaerData.totalClasses = Number(fravaerData.matches[2]);
+      fravaerData.currentFravær = (fravaerData.currentClasses / fravaerData.totalClasses) * 100;
+      fravaerData.possibleAmount = Math.floor(fravaerData.totalClasses * (desiredAmount / 100));
     }
   });
+  }
+
 
   const updateDesired = (e) => {
-    desiredAmount = e.target.value;
-    heleAaret.possibleAmount = Math.floor(heleAaret.totalClasses * (desiredAmount / 100));
-    opgjort.possibleAmount = Math.floor(opgjort.totalClasses * (desiredAmount / 100));
+    fravaerData.possibleAmount = Math.floor(fravaerData.totalClasses * (desiredAmount / 100));
   };
 </script>
 
 <h1 class="mb-4 text-3xl font-bold">Fravær Regner</h1>
-<div class="w-full max-w-xl rounded-lg bg-base-200 p-4 shadow-md sm:p-6 md:p-8">
-  <label for="procent-range" class="mb-2 block font-medium">Hvor meget procent fravær vil du ende med?</label>
-  <input on:input={updateDesired} id="procent-range" type="range" value="10" class="range range-primary" />
-  {#if heleAaret}
+<label for="procent-range" class="mb-2 block font-medium">Hvor meget procent fravær vil du ende med?</label>
+<input on:input={updateDesired} bind:value={desiredAmount} id="procent-range" type="range"  class="range range-primary" />
+
+<label for="fravaerType" class="mb-2 block font-medium">Skal den udregne på baggrund af dit opgjorte fravær eller for hele året?</label>
+<select on:change={getData} bind:value={fravaerType} id="fravaerType" class="select select-bordered w-full">
+  <option value="opgjort">Opgjort</option>
+  <option value="heleåret">Hele året</option>
+</select>
+
+<div class="flex">
+<div class="w-full max-w-xl rounded-lg bg-base-200 p-4 shadow-md sm:p-6 md:p-8 m-2">
+  <h2 class="text-2xl font-bold">{fravaerType}</h2>
+
+  {#if fravaerData}
     <p class="mt-2 text-xl lg:text-2xl">Du vil ende med <span class="font-bold">{desiredAmount}%</span> fravær</p>
     <p class="mt-2 text-xl lg:text-2xl">
-      Så kan du i alt pjække <span class="font-bold">{heleAaret.possibleAmount.toLocaleString("da")}</span> moduler
+      Så kan du i alt pjække <span class="font-bold">{fravaerData.possibleAmount.toLocaleString("da")}</span> moduler
     </p>
-    {#if heleAaret.possibleAmount - heleAaret.currentClasses < 0}
+    {#if fravaerData.possibleAmount - fravaerData.currentClasses < 0}
       <p class="mt-2 text-xl lg:text-2xl">
         Du har allerede pjækket <span class="font-bold"
-          >{Math.abs(heleAaret.possibleAmount - heleAaret.currentClasses).toLocaleString("da")}</span
+          >{Math.abs(fravaerData.possibleAmount - fravaerData.currentClasses).toLocaleString("da")}</span
         >
         moduler for meget
       </p>
     {:else}
       <p class="mt-2 text-xl lg:text-2xl">
         Du kan pjække <span class="font-bold"
-          >{(heleAaret.possibleAmount - heleAaret.currentClasses).toLocaleString("da")}</span
+          >{(fravaerData.possibleAmount - fravaerData.currentClasses).toLocaleString("da")}</span
         > moduler mere
       </p>
     {/if}
     <br />
     <p class="mt-2 text-2xl">Stats</p>
     <p class="mt-2 text-xl">
-      Antal fraværende moduler: <span class="font-bold">{heleAaret.currentClasses.toLocaleString("da")}</span>
+      Antal fraværende moduler: <span class="font-bold">{fravaerData.currentClasses.toLocaleString("da")}</span>
     </p>
     <p class="mt-2 text-xl">
-      Totalt antal moduler: <span class="font-bold">{heleAaret.totalClasses.toLocaleString("da")}</span>
+      Totalt antal moduler: <span class="font-bold">{fravaerData.totalClasses.toLocaleString("da")}</span>
     </p>
     <p class="mt-2 text-xl">
-      Fravær procent: <span class="font-bold">{heleAaret.currentFravær.toLocaleString("da")}%</span>
+      Fravær procent: <span class="font-bold">{fravaerData.currentFravær.toLocaleString("da")}%</span>
     </p>
   {/if}
+</div>
 </div>
