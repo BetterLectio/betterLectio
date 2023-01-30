91e15ead6572a1e2381f9dcd4606a25a8a38baf5
jona799t <jonathan.b.skettrup@gmail.com>
2023-01-30 20:03:49 +0100

    Fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,37 @@
-import adapter from "@sveltejs/adapter-auto";
-import preprocess from "svelte-preprocess";
+// Virker med electron: 
+// Virker med netlify:
+import 'dotenv/config';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  kit: {
-    adapter: adapter(),
-  },
-  preprocess: [
-    preprocess({
-      postcss: true,
-    }),
-  ],
-};
+import adapterStatic from '@sveltejs/adapter-static';
+import adapterAuto from "@sveltejs/adapter-auto";
+import preprocess from 'svelte-preprocess';
 
+let config;
+if (process.env.BUILD_TYPE === 'app') {
+	/** @type {import('@sveltejs/kit').Config} */
+	config = {
+		// Consult https://github.com/sveltejs/svelte-preprocess
+		// for more information about preprocessors
+		preprocess: preprocess(),
+
+		kit: {
+			adapter: adapterStatic({
+				fallback: 'index.html',
+			}),
+			prerender: { entries: [] },
+		},
+	};
+} else {
+	/** @type {import('@sveltejs/kit').Config} */
+	config = {
+		kit: {
+		adapter: adapterAuto(),
+		},
+		preprocess: [
+			preprocess({
+				postcss: true,
+			}),
+		],
+	};
+}
 export default config;
