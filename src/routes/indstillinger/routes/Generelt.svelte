daab02b216061159c69310ec541b43e5dde57332
Asguho <78410939+Asguho@users.noreply.github.com>
2023-01-31 20:40:58 +0100

    Update Generelt.svelte

--- src/routes/indstillinger/routes/Generelt.svelte
+++ src/routes/indstillinger/routes/Generelt.svelte
@@ -54,7 +54,7 @@
   </div>
 </div>
 
-<div class="mt-4 rounded-lg bg-base-200 p-4">
+<!-- <div class="mt-4 rounded-lg bg-base-200 p-4">
   <div class="form-control">
     <span class="mb-2 text-lg font-bold">Skema siden</span>
 
@@ -63,7 +63,7 @@
       <input type="checkbox" class="checkbox " bind:checked={$indstillinger.skema.classesWithDiffrentColors} />
     </label>
   </div>
-</div>
+</div> -->
 
 <h2 class="mt-4 font-mono opacity-20 transition-all duration-300 hover:opacity-100">
   BetterLectio version: {__APP_VERSION__}
