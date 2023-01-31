3562403c136ba0de41a9949d4db99d342c4d8563
jona799t <jonathan.b.skettrup@gmail.com>
2023-01-31 22:07:48 +0100

    Fix /discord

--- src/routes/discord/+page.svelte
+++ src/routes/discord/+page.svelte
@@ -1,5 +1,8 @@
 <script>
-    window.location.href = "https://discord.gg/uj4xtSUnKN";
-  </script>
-  
-  <p>Er du ikke blevet omdirigeret? Så klik <a class="btn-primary btn-xs btn" href="https://discord.gg/uj4xtSUnKN">her</a></p>
+	import { onMount } from 'svelte';
+	onMount(async () => {
+        window.location.href = "https://discord.gg/uj4xtSUnKN";
+	});
+</script>
+
+<p>Er du ikke blevet omdirigeret? Så klik <a class="btn-primary btn-xs btn" href="https://discord.gg/uj4xtSUnKN">her</a></p>
