bd22162ca1d69c358f3f2df2f06dbf0eeef08560
Asguho <78410939+Asguho@users.noreply.github.com>
2023-01-31 20:18:27 +0100

    added colored classes

--- src/routes/skema/+page.svelte
+++ src/routes/skema/+page.svelte
@@ -2,6 +2,7 @@
   import { get } from "../../components/http.js";
   import Calendar from "@event-calendar/core";
   import TimeGrid from "@event-calendar/time-grid";
+  import { indstillinger } from "../../components/store.js";
 
   import { cookieInfo } from "../../components/CookieInfo";
 
@@ -199,7 +200,7 @@
         start: new Date(`${start.år}-${start.måned}-${start.dag}T${start.tidspunkt}`),
         end: new Date(`${slut.år}-${slut.måned}-${slut.dag}T${slut.tidspunkt}`),
         id: modul["absid"],
-        backgroundColor: className,
+        backgroundColor: $indstillinger?.classesWithDiffrentColors ? className : stringToHex(modul["hold"]),
       };
       let isAdded = false;
       for (let i = 0; i < addedEventsId.length; i++) {
@@ -225,6 +226,7 @@
       skema = {};
     }
     await get(`/skema?id=${skemaId}&uge=${globalWeek}&år=${globalYear}`).then((data) => {
+      console.log(data);
       skema[globalYear + "" + globalWeek] = data;
 
       options.slotMinTime = parseInt(
@@ -269,6 +271,20 @@
         globalWeek++;
         break;
     }
+  }
+  function stringToHex(string) {
+    var hash = 0;
+    if (string.length === 0) return hash;
+    for (var i = 0; i < string.length; i++) {
+      hash = string.charCodeAt(i) + ((hash << 5) - hash);
+      hash = hash & hash;
+    }
+    var color = "#";
+    for (var i = 0; i < 3; i++) {
+      var value = (hash >> (i * 8)) & 255;
+      color += ("00" + value.toString(16)).substr(-2);
+    }
+    return color;
   }
 
   function addButtonsToDagsnoter(year, week) {
