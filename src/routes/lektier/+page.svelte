fafc4909232e015be014c1e81e11f2ee99ce4948
victor <victordj587@gmail.com>
2023-02-01 14:17:16 +0100

    lektier per dag

--- src/routes/lektier/+page.svelte
+++ src/routes/lektier/+page.svelte
@@ -1,40 +1,51 @@
 <script>
-  /**
-     TODO:
-        - Gør så man kan downloade filer uden at blive redirectet til modul siden
-        - Måske gør så man får al teksten og derfor ikke behøver at klikke på lektien
-     */
   import { get } from "../../components/http.js";
   import { lektier } from "../../components/store.js";
 
   get("/lektier").then((data) => {
     $lektier = data;
   });
+
+  function separateAssignmentsByDays() {
+    let datoer = [];
+    $lektier.forEach((lektie) => {
+      if (!datoer.includes(lektie.aktivitet.tidspunkt.split(" ")[0])) {
+        datoer.push(lektie.aktivitet.tidspunkt.split(" ")[0]);
+      }
+      console.log(datoer);
+    });
+    return datoer;
+  }
+
+  $: separateAssignmentsByDays();
 </script>
 
 <body>
   <h1 class="text-3xl font-bold">Lektier</h1>
   <h3 class="mb-4 text-xl">Her kan du se dine lektier for de kommende 14 dage</h3>
   {#if $lektier.length > 0}
-    <ul class="list">
-      {#if $lektier}
+    {#each Object.values(separateAssignmentsByDays()) as datoer}
+      <h2 class="mb-2 mt-4 text-lg font-bold">{datoer}</h2>
+      <ul class="list">
         {#each $lektier as lektie}
-          <a href="/modul?absid={lektie.aktivitet.absid}">
-            <li class="element border-l-0 border-primary transition-all duration-100 hover:border-l-4">
-              <div>
-                <p>
-                  <span class="font-bold"
-                    >{lektie.aktivitet.navn != null ? lektie.aktivitet.navn + " · " : ""}{lektie.aktivitet.hold}</span
-                  >
-                  ({lektie.aktivitet.tidspunkt})
-                </p>
-                <p>{lektie.lektier.beskrivelse}</p>
-              </div>
-            </li>
-          </a>
+          {#if lektie.aktivitet.tidspunkt.split(" ")[0] == datoer}
+            <a href="/modul?absid={lektie.aktivitet.absid}">
+              <li class="element border-l-0 border-primary transition-all duration-100 hover:border-l-4">
+                <div>
+                  <p>
+                    <span class="font-bold"
+                      >{lektie.aktivitet.navn != null ? lektie.aktivitet.navn + " · " : ""}{lektie.aktivitet.hold}</span
+                    >
+                    ({lektie.aktivitet.tidspunkt})
+                  </p>
+                  <p>{lektie.lektier.beskrivelse}</p>
+                </div>
+              </li>
+            </a>
+          {/if}
         {/each}
-      {/if}
-    </ul>
+      </ul>
+    {/each}
   {:else}
     <p>Du har ingen kommende lektier de næste 14 dage</p>
   {/if}
