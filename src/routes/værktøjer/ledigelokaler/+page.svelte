78f443e40d63500346c459b0be1d8630e88916f2
victor <victordj587@gmail.com>
2023-02-24 15:12:54 +0100

    Ledige lokaler klikbar

--- src/routes/værktøjer/ledigelokaler/+page.svelte
+++ src/routes/værktøjer/ledigelokaler/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import { get } from "../../../components/http.js";
-  import { lokaler } from "../../../components/store.js";
+  import { lokaler, informationer } from "../../../components/store.js";
+  import { goto } from "$app/navigation";
 
   get("/ledige_lokaler").then((data) => {
     let _lokaler = { ledige: [], optagede: [] };
@@ -13,13 +14,34 @@
     });
     $lokaler = _lokaler;
   });
+
+  let lokalerAndRoomId = {};
+
+  get("/informationer").then((data) => {
+    $informationer = data;
+    lokalerAndRoomId = data.lokaler;
+  });
+
+  function findRoomIdAndRedirect(roomName) {
+    // the lokalerAndRoomId is an object with the room name as key and the room id as value
+    // so we can just get the value by using the room name as key and search for it in the object
+
+    for (const [key, value] of Object.entries(lokalerAndRoomId)) {
+      if ("Lokale " + key == roomName) {
+        goto(`/skema?id=${value.slice(0, 1) + value.slice(2)}`); //filter the value to not have "O" in the second index of the string then redirect to the skema page
+      }
+    }
+  }
 </script>
 
 {#if $lokaler}
   <ul class="list mb-4">
     <h1 class="pb-2 text-2xl font-bold">Ledige lokaler</h1>
     {#each $lokaler.ledige as lokale}
-      <li class="btn btn-success mb-2 flex h-fit scale-99 hover:scale-100 md:justify-between">
+      <li
+        class="btn-success btn mb-2 flex h-fit scale-99 hover:scale-100 md:justify-between"
+        on:click={findRoomIdAndRedirect(lokale.lokale)}
+      >
         <p class="text-lg font-bold">{lokale.lokale}</p>
       </li>
     {/each}
@@ -31,7 +53,10 @@
   <ul class="list mb-4">
     <h1 class="pb-2 text-2xl font-bold">Optagede lokaler</h1>
     {#each $lokaler.optagede as lokale}
-      <li class="btn-error btn mb-2 flex h-fit scale-99 justify-between hover:scale-100">
+      <li
+        class="btn-error btn mb-2 flex h-fit scale-99 justify-between hover:scale-100"
+        on:click={findRoomIdAndRedirect(lokale.lokale)}
+      >
         <p class="text-lg font-bold">{lokale.lokale}</p>
       </li>
     {/each}
