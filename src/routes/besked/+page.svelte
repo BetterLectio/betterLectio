c1fe365ec7404210b3d52b015730fbef95ad664e
victor <victordj587@gmail.com>
2023-02-28 09:51:28 +0100

    update mesage in mesage

--- src/routes/besked/+page.svelte
+++ src/routes/besked/+page.svelte
@@ -13,6 +13,8 @@
     cookie = data;
   });
 
+  let updating = false;
+
   const md = new MarkdownIt();
 
   const beskedId = new URLSearchParams(window.location.search).get("id");
@@ -57,6 +59,16 @@
     get("/besked?id=" + beskedId).then((data) => {
       besked = data.beskeder;
       modtagere = data.modtagere;
+    });
+  }
+
+  function updateBesked() {
+    updating = true;
+    addNotification("opdaterer beskedtråden", "alert-info");
+    get("/besked?id=" + beskedId).then((data) => {
+      besked = data.beskeder;
+      modtagere = data.modtagere;
+      updating = false;
     });
   }
 
@@ -113,7 +125,27 @@
 </label>
 
 {#if besked}
-  <h1 class="mb-2 text-3xl font-bold">{besked[0].titel}</h1>
+  <div class="flex-rox flex w-full justify-between">
+    <h1 class="mb-2 text-3xl font-bold">{besked[0].titel}</h1>
+    <button class={`btn-circle btn ml-4 ${updating ? "animate-spin" : ""}`} on:click={updateBesked}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        fill="currentColor"
+        class="bi bi-arrow-repeat"
+        viewBox="0 0 16 16"
+      >
+        <path
+          d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"
+        />
+        <path
+          fill-rule="evenodd"
+          d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"
+        />
+      </svg>
+    </button>
+  </div>
   <p class="mb-4"><strong>Modtagere:</strong> {modtagere}</p>
   <!-- Måske en linje til at seperere beskeder-->
   <div class="divide-y divide-base-content rounded-none md:divide-none md:rounded-lg md:bg-base-200 md:p-4">
