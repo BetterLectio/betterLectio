376da87980bb86f09182678a63728dda77b99e7c
victor <victordj587@gmail.com>
2023-03-01 18:08:10 +0100

    mostly done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -288,26 +288,54 @@
 						</p>
 
 						<a
-								href="https://github.com/BetterLectio"
-								target="_blank"
-								class="btn mt-4"
-								in:fly={{ duration: 1000, y: 0, x: -30, easing: quintInOut, delay: 1600 }}
-								><svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="20"
-									height="20"
-									fill="currentColor"
-									class="bi bi-discord mr-2"
-									viewBox="0 0 16 16"
-								>
-									<path
-										d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"
-									/>
-								</svg>Github</a
+							href="https://github.com/BetterLectio"
+							target="_blank"
+							class="btn mt-4"
+							in:fly={{ duration: 1000, y: 0, x: -30, easing: quintInOut, delay: 1600 }}
+							><svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="20"
+								height="20"
+								fill="currentColor"
+								class="bi bi-discord mr-2"
+								viewBox="0 0 16 16"
 							>
+								<path
+									d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"
+								/>
+							</svg>Github</a
+						>
 					</div>
 				</div>
 			</div>
+
+			{#if stats}
+			<div class="w-full flex justify-center">
+				<div id="stats" class="mb-40 bg-base-200 rounded-xl p-4 lg:w-fit">
+					<h1 class="mb-6 text-4xl font-extrabold">Hvor mange bruger BetterLectio?</h1>
+					<div class="flex flex-col lg:flex-row">
+						<div class="stats shadow">
+							<div class="stat">
+								<div class="stat-title">Elever</div>
+								<div class="stat-value text-6xl">
+									<Countup value={stats["antal_brugere"]} duration={3000} format={false} />
+								</div>
+								<div class="stat-desc">Elever der bruger BetterLectio</div>
+							</div>
+						</div>
+						<div class="stats shadow lg:ml-6 mt-6 lg:mt-0">
+							<div class="stat">
+								<div class="stat-title">Skoler</div>
+								<div class="stat-value">
+									<Countup value={stats["antal_skoler"]} duration={3000} format={false} />
+								</div>
+								<div class="stat-desc">Skoler med BetterLectio</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			{/if}
 
 			<h1 class="mb-2 text-4xl font-extrabold lg:text-center">Se BetterLectio i aktion</h1>
 			<div class="aspect-w-16 aspect-h-9 max-w-7xl mx-auto mb-40">
@@ -318,34 +346,6 @@
 					frameborder="0"
 				/>
 			</div>
-
-			{#if stats}
-				<div id="stats" class="mb-40 bg-base-200 rounded-xl p-4">
-					<h1 class="mb-2 text-4xl font-extrabold">Hvor mange bruger BetterLectio?</h1>
-					<p class="mb-2">
-						Selvom vi stadig er under konstant udvikling har vi alligevel fået ret mange brugere
-					</p>
-					<div class="flex flex-col lg:flex-row ">
-						<!-- make 3 cards thae are nex to each other-->
-						<div class="bg-base-300 p-4 lg:mr-4 mb-4 lg:mb-0 rounded-lg w-full">
-							<span class="font-mono text-6xl">
-								<div class="flex items-center justify-center object-center">
-									<Countup value={stats["antal_brugere"]} duration={1000} format={false} />
-								</div>
-								<h3 class="text-2xl text-center">Elever</h3>
-							</span>
-						</div>
-						<div class="bg-base-300 p-4 lg:mr-4 mb-4 lg:mb-0 rounded-lg w-full">
-							<span class="font-mono text-6xl text-center">
-								<div class="flex items-center justify-center object-center">
-									<Countup value={stats["antal_skoler"]} duration={1000} format={false} />
-								</div>
-								<h3 class="text-2xl text-center">Skoler</h3>
-							</span>
-						</div>
-					</div>
-				</div>
-			{/if}
 
 			{#if loaded}
 				<div id="downloads" class="mb-40 bg-base-200 rounded-xl p-4">
