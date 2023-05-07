350a2ed013eed371fcfb7fd8d82cd48fa0539cc8
Asguho <78410939+Asguho@users.noreply.github.com>
2023-05-07 11:11:03 +0200

    cleaned up

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-import { readFileSync } from 'fs';
-import { fileURLToPath } from 'url';
- 
-const file = fileURLToPath(new URL('package.json', import.meta.url));
-const json = readFileSync(file, 'utf8');
-const pkg = JSON.parse(json);
-
-
-const config = {
-  plugins: [sveltekit()],
-  define: {
-    '__APP_VERSION__': JSON.stringify(pkg.version),
-  }
-};
-
-export default config;
+export default defineConfig({
+	plugins: [sveltekit()]
+});
