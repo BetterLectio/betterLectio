c1598ef63e902758d0db2de6a00822579a488e42
victor <victordj587@gmail.com>
2023-05-09 08:45:24 +0200

    safe url ecode and decode

--- src/lib/js/CookieInfo.js
+++ src/lib/js/CookieInfo.js
@@ -5,12 +5,7 @@ export async function cookieInfo() {
     window.location.href.split("?")[0].split("/").at(-1) != ""
   ) {
     console.log("Redirect");
-    const transformedLink = window.location.href
-      .split("/")[3]
-      .replace("/", "%3")
-      .replace("?", "%3F")
-      .replace("=", "%3d")
-      .replace(`"`, "%22");
+    const transformedLink = encodeURIComponent(window.location.href)
     window.location.href = "/auth?redirect=" + transformedLink;
   } else {
     try {
--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -13,6 +13,7 @@
   let options = { "": "" };
 
   let redirectTo = new URLSearchParams(window.location.search).get("redirect");
+  decodeURIComponent(redirectTo) == "null" ? (redirectTo = "/forside") : (redirectTo = decodeURIComponent(redirectTo));
 
   function tryLoginInWithCookie() {
     if (localStorage.getItem("lectio-cookie") || localStorage.getItem("lectio-cookie") != "null") {
@@ -24,7 +25,7 @@
         res.json().then((data) => {
           if (data?.valid) {
             console.log("Logged in with cookie");
-            goto("/forside");
+            window.location.href = decodeURIComponent(redirectTo);
           } else {
             console.log("Cookie not valid.", "valitation:", data);
           }
@@ -102,12 +103,8 @@
         });
         progress.classList.remove("loading");
         reloadData();
-        if (redirectTo) {
-          const originalLink = redirectTo.replace("%3", "/").replace("%3F", "?").replace("%3d", "=").replace("%22", `"`);
-          goto(originalLink);
-        } else {
-          goto("/forside");
-        }
+        const originalLink = decodeURIComponent(redirectTo);
+        window.location.href = originalLink;
       }
     }
   }
@@ -143,42 +140,42 @@
     <p class="py-4">Du skal udfylde alle felterne for at logge ind.</p>
   </div>
 </div>
-<div use:getCachedSchool class="flex justify-center md:h-[75vh] items-center">
-  <div class="bg-base-200 h-fit p-4 rounded-2xl shadow-lg">
-    <h1 class="font-bold text-xl">Log ind med din Lectio konto</h1>
+<div use:getCachedSchool class="flex items-center justify-center md:h-[75vh]">
+  <div class="h-fit rounded-2xl bg-base-200 p-4 shadow-lg">
+    <h1 class="text-xl font-bold">Log ind med din Lectio konto</h1>
     <div class="divider mt-0" />
     <form action="login" autocomplete="on" method="post">
       <div class="form-control w-full max-w-xl">
-        <label class="input-group input-group-horizontal mb-2">
-          <span class="w-28 bg-base-100 border-r-2 border-base-200">Brugernavn</span>
+        <label class="input-group-horizontal input-group mb-2">
+          <span class="w-28 border-r-2 border-base-200 bg-base-100">Brugernavn</span>
           <input
             type="text"
             name="username"
             id="username-field"
             placeholder="Skriv her"
-            class="input font-semibold autofill:shadow-[inset_0_0_0px_1000px_hsl(var(--b1))] autofill:border-0 input-sm w-[calc(100%-7rem)]"
+            class="input input-sm w-[calc(100%-7rem)] font-semibold autofill:border-0 autofill:shadow-[inset_0_0_0px_1000px_hsl(var(--b1))]"
             bind:value={brugernavn}
           />
         </label>
-        <label class="input-group input-group-horizontal mb-2">
-          <span class="w-28 bg-base-100 border-r-2 border-base-200">Kodeord</span>
+        <label class="input-group-horizontal input-group mb-2">
+          <span class="w-28 border-r-2 border-base-200 bg-base-100">Kodeord</span>
           <input
             type="password"
             name="adgangskode"
             id="adgangskode"
             placeholder="Skriv her"
-            class="input autofill:shadow-[inset_0_0_0px_1000px_hsl(var(--b1))] autofill:border-0 font-semibold input-sm w-[calc(100%-7rem)]"
+            class="input input-sm w-[calc(100%-7rem)] font-semibold autofill:border-0 autofill:shadow-[inset_0_0_0px_1000px_hsl(var(--b1))]"
             bind:value={adgangskode}
             on:keypress={handleEnterLogin}
           />
         </label>
-        <label class="input-group input-group-horizontal mb-2">
-          <span class="w-28 bg-base-100 border-r-2 border-base-200">Skole</span>
+        <label class="input-group-horizontal input-group mb-2">
+          <span class="w-28 border-r-2 border-base-200 bg-base-100">Skole</span>
           <select
             name="skole"
             id="skole"
             placeholder="Vælg din skole"
-            class="select select-sm py-0 w-[calc(100%-7rem)]"
+            class="select select-sm w-[calc(100%-7rem)] py-0"
             bind:value={skole_id}
           >
             <option disabled selected> Vælg din skole </option>
