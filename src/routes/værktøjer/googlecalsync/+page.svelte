0a12391dd0715d4ba728ae636cce1028d5d4f7db
victor <victordj587@gmail.com>
2023-08-01 19:00:27 +0200

    update

--- src/routes/værktøjer/googlecalsync/+page.svelte
+++ src/routes/værktøjer/googlecalsync/+page.svelte
@@ -194,12 +194,16 @@
 <h1 class="heading">Google Kalender-synkronisering</h1>
 
 {#if !loggedin}
-  <div class="alert alert-warning mb-4">
-    <!-- prettier-ignore -->
-    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24" ><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg >
-    <span
-      >Adgang er på nuværende tidspunkt begrænset til udviklere. Hvis du er interesseret i at teste funktionen, så kontakt os på discorden.</span
+  <div class="alert alert-info mb-4">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-6 h-6"
+      ><path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+      /></svg
     >
+    <span>Vi arbejder på at blive godkendt af Google. Indtil da vil der være en advarsel om, at siden ikke er verificeret.</span>
   </div>
 {:else}
   <p class="text-sm md:w-96 italic my-4 border-l-2 pl-2 border-warning">
