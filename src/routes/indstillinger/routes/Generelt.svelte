ecfebff737581f9a9d85c46f5aba8e38f716c1b0
jona799t <jonathan.b.skettrup@gmail.com>
2023-08-01 18:53:09 +0200

    Termin skifter i indstillinger

--- src/routes/indstillinger/routes/Generelt.svelte
+++ src/routes/indstillinger/routes/Generelt.svelte
@@ -1,5 +1,6 @@
 <script>
   import { indstillinger, hold } from "$lib/js/store";
+  import { get } from "$lib/js/http.js";
 
   let windowWidth = window.innerWidth;
 
@@ -22,6 +23,28 @@
 
     $hold.push({ forkortelse: forkortelse, fag: fag });
     $hold = $hold;
+  }
+
+  let terminer;
+  get("/terminer").then((data) => {
+    terminer = data;
+  });
+
+  function aendreTermin(btn) {
+    terminer = null;
+
+    let id = btn.srcElement.id.toString();
+    get(`/aendre_termin?id=${id}`).then((data) => {
+      if (data.success) {
+        terminer.selected = id;
+      } else {
+        alert("Der skete en fejl ved ændringen af terminet");
+      }
+    });
+
+    get("/terminer").then((data) => {
+      terminer = data;
+    });
   }
 </script>
 
@@ -82,4 +105,27 @@
     <span class="label-text">Fag Oversætter</span>
     <input type="checkbox" class="checkbox" bind:checked={$indstillinger.brugHoldOversætter} />
   </label>
+</div>
+
+<div class="mt-4 rounded-lg bg-base-200 p-4">
+  <div class="form-control">
+    <span class="mb-2 text-lg font-bold">Termin</span>
+    <p>Skift dit termin</p>
+  </div>
+  {#if terminer}
+    {#each Object.entries(terminer.terminer) as termin}
+      <label class="label cursor-pointer">
+        <span class="label-text">{termin[1]}</span>
+        <input
+          type="checkbox"
+          class="checkbox"
+          checked={termin[0] == terminer.selected ? "checked" : ""}
+          id={termin[0]}
+          on:click={aendreTermin}
+        />
+      </label>
+    {/each}
+  {:else}
+    <span class="loading loading-dots loading-lg" />
+  {/if}
 </div>
