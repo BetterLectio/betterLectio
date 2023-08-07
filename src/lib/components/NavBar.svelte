3724b67e85dc1b1ca5ccc6036ffb90be44f5cdcc
victor <victordj587@gmail.com>
2023-08-07 15:39:39 +0200

    sidebar ui fix

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -36,7 +36,7 @@
         </div>
 
         <p
-          class="btn-ghost btn mx-0 hidden text-2xl normal-case sm:flex"
+          class="btn-ghost btn mx-0 hidden text-2xl normal-case sm:flex group"
           on:keypress={() => {
             /*makes error go away*/
           }}
@@ -44,7 +44,13 @@
             goto("/forside");
           }}
         >
-          <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" height="40" class="mx-0 fill-current p-0">
+          <svg
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            height="40"
+            class="mx-0 fill-current p-0 group-hover:rotate-12 duration-200"
+          >
             <path
               d="M428.033,162.858C422.975,72.232,347.87,0,256,0S89.025,72.232,83.967,162.858c-0.323,3.179-0.489,6.402-0.489,9.663c0,15.543,0,320.162,0,322.783c0,9.22,7.475,16.696,16.696,16.696h111.304c119.87,0,217.043-97.174,217.043-217.043V172.522C428.522,169.261,428.356,166.037,428.033,162.858z M256,33.391c42.163,0,79.93,18.923,105.464,48.641c-8.722-2.662-17.967-4.119-27.551-4.119c-32.296,0-60.834,16.291-77.913,41.06c-17.079-24.77-45.617-41.06-77.913-41.06c-9.584,0-18.829,1.457-27.551,4.119C176.07,52.314,213.837,33.391,256,33.391z M163.169,360.642c-5.842-7.12-16.37-8.174-23.494-2.326c-6.988,5.731-14.635,10.267-22.804,13.595v-97.801c26.505,36.201,66.686,61.636,112.937,68.728c-18.13,49.489-60.64,87.32-112.937,98.783v-34.418h-0.001c16.082-4.569,30.884-12.325,43.979-23.061C167.978,378.293,169.017,367.772,163.169,360.642z M395.13,294.957c0,101.261-82.386,183.652-183.652,183.652H116.87v-2.97c70.214-12.524,126.789-63.872,147.277-131.009c53.767-2.53,101.081-29.682,130.984-70.521V294.957z M256,311.652c-76.718,0-139.13-62.413-139.13-139.13c0-33.75,27.462-61.217,61.217-61.217s61.217,27.468,61.217,61.217c0,9.22,7.475,16.696,16.696,16.696s16.696-7.475,16.696-16.696c0-33.75,27.462-61.217,61.217-61.217c33.755,0,61.217,27.468,61.217,61.217C395.13,249.239,332.718,311.652,256,311.652z"
             />
@@ -204,9 +210,11 @@
       </div>
     </div>
   </div>
-  <div class="drawer-side">
-    <label for="menu-drawer" class="drawer-overlay" />
-    <ul class="menu w-80 relative z-50 h-screen bg-base-100/80 p-4 pt-20 text-base-content flex flex-col justify-between backdrop-blur-md">
+  <div class="drawer-side translate-y-16">
+    <label for="menu-drawer" class="drawer-overlay -translate-y-16 z-40" />
+    <ul
+      class="menu w-80 relative z-50 min-h-screen h-fit bg-base-100/80 p-4 text-base-content pb-20 flex flex-col justify-between backdrop-blur-md flex-nowrap"
+    >
       {#if $brugeren && localStorage.getItem("lectio-cookie") && cookie}
         <div>
           <span class="md:hidden">
@@ -219,7 +227,7 @@
             </div>
           </span>
           <div class="divider" />
-          <div>
+          <div class="overflow-y-scroll">
             <li on:click={handleClick}>
               <a href="/skema"
                 ><svg
