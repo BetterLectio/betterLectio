289f4ebd51189203f89e40f1971e088004bc82e9
Asger Finding <asger.finding@gmail.com>
2023-08-16 13:28:44 +0200

    feat: tilføj neddæmpet mørke temafarver

--- src/lib/components/Themes.svelte
+++ src/lib/components/Themes.svelte
@@ -310,6 +310,39 @@
     </div>
   </button>
 </li>
+<li>
+  <button
+    class="mx-0 my-1 p-0 hover:scale-110"
+    data-set-theme="business"
+    data-act-class="ACTIVECLASS"
+    on:click={() => {
+      if (localStorage.getItem("themeOverride")) {
+        localStorage.removeItem("themeOverride");
+        location.reload();
+      }
+    }}
+  >
+    <div
+      class="w-full overflow-hidden rounded-lg text-left outline outline-2 outline-offset-2 outline-base-content"
+      data-set-theme="business"
+      data-act-class="outline"
+    >
+      <div data-theme="business" class="w-[182px] cursor-pointer bg-base-100 font-sans text-base-content">
+        <div class="grid grid-cols-5 grid-rows-3">
+          <div class="col-span-5 row-span-3 row-start-1 flex gap-1 py-3 px-4">
+            <div class="flex-grow text-sm font-bold">Neddæmpet</div>
+            <div class="flex flex-shrink-0 flex-wrap gap-1">
+              <div class="w-2 rounded bg-primary" />
+              <div class="w-2 rounded bg-secondary" />
+              <div class="w-2 rounded bg-accent" />
+              <div class="w-2 rounded bg-neutral" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </button>
+</li>
 
 {#await themes then}
   {#key themes}
