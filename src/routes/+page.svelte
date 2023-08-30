7e8c9589b2c13f9d70496c055b11ab4c5625b845
Asger Finding <asger.finding@gmail.com>
2023-08-30 09:44:56 +0200

    fix: better user feedback for cookie auth

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,27 +4,27 @@
 	import { goto } from '$app/navigation';
 	import { onMount } from 'svelte';
 
-	let step = 'indlæser';
+	let step = 'Indlæser';
 	onMount(() => {
 		try {
-			step = 'finding cookie';
+			step = 'Finder session';
 			if (localStorage.getItem('lectio-cookie')) {
-				step = 'validating cookie';
+				step = 'Validerer session';
 				fetch(`${api}/check-cookie`, { headers: { 'lectio-cookie': localStorage.getItem('lectio-cookie') } }).then(res => {
-					step = 'checking cookie';
+					step = 'Tjekker session';
 					res.json().then(data => {
 						if (data?.valid) {
-							step = 'cookie valid';
+							step = 'Session er gyldig';
 							console.log('Logged in with cookie');
 							goto('forside');
 						} else {
-							step = 'cookie invalid';
+							step = 'Session er ugyldig';
 							goto('auth');
 						}
 					});
 				});
 			} else {
-				step = 'no cookie';
+				step = 'Ingen cookie';
 				goto('auth');
 			}
 		} catch (error) {
