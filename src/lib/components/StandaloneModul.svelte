dd4fc08b69f534fe105106d217a9d5c26f5a943a
victor <victordj587@gmail.com>
2023-09-04 00:10:16 +0200

    holdOversætterNy i StandaloneModul.svelte

--- src/lib/components/StandaloneModul.svelte
+++ src/lib/components/StandaloneModul.svelte
@@ -1,8 +1,23 @@
 <script>
 	import { getModulColor } from '$lib/js/LectioUtils.js';
+	import { holdOversætterNy } from '$lib/js/HoldOversætter.js';
+	import { indstillinger } from '$lib/js/store.js';
+
 
 	export let modul = {};
 	export let renderWithColor = true;
+	let oversatHold = modul.hold;
+
+	async function getModulHold() {
+		if (modul.hold && $indstillinger.brugHoldOversætter) {
+			// eslint-disable-next-line require-atomic-updates
+			oversatHold = await holdOversætterNy(modul.hold_id);
+			console.log(oversatHold);
+		}
+	}
+
+	// if the modul has changed, run the function
+	$: if (modul) getModulHold();
 </script>
 
 {#if !modul.absid.includes('privat')}
@@ -10,7 +25,7 @@
 		{#if modul.navn}
 			{#if modul.andet}
 				<div class="tooltip flex justify-center" data-tip="Har indhold">
-					<h1 class="text-xl font-bold">{modul.navn} {modul.hold ? modul.hold : ''}</h1>
+					<h1 class="text-xl font-bold">{modul.navn} {modul.hold ? oversatHold : ''}</h1>
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						width="16"
@@ -23,10 +38,10 @@
 					</svg>
 				</div>
 			{:else}
-				<h1 class="text-xl font-bold">{modul.navn} {modul.hold ? modul.hold : ''}</h1>
+				<h1 class="text-xl font-bold">{modul.navn} {modul.hold ? oversatHold : ''}</h1>
 			{/if}
 		{:else}
-			<h1 class="text-xl font-bold">{modul.hold}</h1>
+			<h1 class="text-xl font-bold">{oversatHold}</h1>
 		{/if}
 		<h1 class="text-sm font-bold">{modul.tidspunkt}</h1>
 		{#if modul.lokale}
@@ -37,9 +52,9 @@
 	<div class="btn mb-4 block h-fit p-2 normal-case border-0 no-animation last:mb-0" style={`background-color:${getModulColor(modul, renderWithColor)}`}>
 		<div class="flex justify-center items-center">
 			{#if modul.navn}
-				<h1 class="text-xl font-bold">{modul.navn} {modul.hold ? modul.hold : ''}</h1>
+				<h1 class="text-xl font-bold">{modul.navn} {modul.hold ? oversatHold : ''}</h1>
 			{:else}
-				<h1 class="text-xl font-bold">{modul.hold}</h1>
+				<h1 class="text-xl font-bold">{oversatHold}</h1>
 			{/if}
 			<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-fill-lock ml-2" viewBox="0 0 16 16">
 				<path d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm-9 8c0 1 1 1 1 1h5v-1a1.9 1.9 0 0 1 .01-.2 4.49 4.49 0 0 1 1.534-3.693C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4Zm7 0a1 1 0 0 1 1-1v-1a2 2 0 1 1 4 0v1a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1v-2Zm3-3a1 1 0 0 0-1 1v1h2v-1a1 1 0 0 0-1-1Z"/>
