a0c907ec820b92a5cc3435d0380ba3787d51df71
Asguho <78410939+Asguho@users.noreply.github.com>
2023-09-14 16:06:45 +0200

    fix: mobile disclaimer

--- src/routes/opgaver/+page.svelte
+++ src/routes/opgaver/+page.svelte
@@ -122,7 +122,7 @@
 
 	</div>
 	{#if selected === 'feedbackOpgaver'}
-		<div class="alert w-fit mt-4 p-2">
+		<div class="alert w-fit mt-4 p-2 flex">
 			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info shrink-0 w-4 h-4"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
 			<span class="text-xs">Der kan være opgaver på denne liste, der ikke har fået feedback, hvis læren har afsluttet opgaven uden at give feedback.</span>
 		</div>
