2b8df76639a8f9021a82a96134a6e172ec720071
Victor Digital <victordj587@gmail.com>
2023-11-10 09:10:21 +0100

    Add Sentry config file and plugin for SvelteKit

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,10 @@
+import { sentrySvelteKit } from "@sentry/sveltekit";
 import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 
-export default defineConfig({ plugins: [sveltekit()] });
+export default defineConfig({ plugins: [sentrySvelteKit({
+  sourceMapsUploadOptions: {
+    org: "betterlectio",
+    project: "javascript-sveltekit"
+  }
+}), sveltekit()] });
