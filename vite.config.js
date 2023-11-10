db4d13e1d35a6a890a497f227cfb6d4049228f6e
Victor Digital <victordj587@gmail.com>
2023-11-10 16:44:45 +0100

    config fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import { sentrySvelteKit } from "@sentry/sveltekit";
+import { sentrySvelteKit } from '@sentry/sveltekit';
 import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 
-export default defineConfig({ plugins: [sentrySvelteKit({
-  sourceMapsUploadOptions: {
-    org: "betterlectio",
-    project: "javascript-sveltekit"
-  }
-}), sveltekit()] });
+export default defineConfig({
+	plugins: [sentrySvelteKit({
+		sourceMapsUploadOptions: {
+			org: 'betterlectio',
+			project: 'javascript-sveltekit'
+		}
+	}), sveltekit()]
+});
