13cbdff59f5cacfb39ca1b1b172c3b85e061a894
victor <victordj587@gmail.com>
2023-11-12 15:26:12 +0100

    source maps :) configure sentryVitePlugin in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,23 @@
-import { sentrySvelteKit } from '@sentry/sveltekit';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 import { sveltekit } from '@sveltejs/kit/vite';
 
-export default defineConfig({
-	plugins: [sentrySvelteKit({
-		sourceMapsUploadOptions: {
-			org: 'betterlectio',
-			project: 'javascript-sveltekit'
-		}
-	}), sveltekit()]
+export default defineConfig(() => {
+	const env = loadEnv('', process.cwd());
+
+	return {
+		build: { sourcemap: true },
+		plugins: [
+			sveltekit(),
+
+			// Put the Sentry vite plugin after all other plugins
+			sentryVitePlugin({
+				org: 'betterlectio',
+				project: 'javascript-sveltekit',
+				authToken: env.VITE_SENTRY_AUTH_TOKEN,
+				cleanArtifacts: true,
+				rewrite: false
+			})
+		]
+	};
 });
