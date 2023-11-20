da1e4bc3051aa4cb4260959baf1e3b95c14522cf
victor <victordj587@gmail.com>
2023-11-20 19:32:49 +0100

    Remove console.error and async from load function

--- src/routes/modul/+page.server.js
+++ src/routes/modul/+page.server.js
@@ -1,9 +1,7 @@
 import { redirect } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageServerLoad} */
-export async function load({ url }) {
-	console.error('url', url);
-
+export function load({ url }) {
 	// the following checks if the user if trying to access a eksamens modul and redirects them to the forside if they are
 	if (url.searchParams.get('absid').includes('proevehold')) throw redirect(302, `/forside`);
 
