8c7498ad2e2efd8f3d4d73c4a6c2441400ae1bf9
victor <victordj587@gmail.com>
2023-12-05 18:23:11 +0100

    Add error handling for fetching lektie data

--- src/routes/lektier/+page.svelte
+++ src/routes/lektier/+page.svelte
@@ -34,6 +34,7 @@
 	const timeRegex
 		= /(?<date>(?:[1-9]|[12][0-9]|3[01])\/(?:[1-9]|1[012])-(?:19|20)\d\d) (?<startTime>(?:[01]?[0-9]|2[0-3]):(?:[0-5][0-9])) til (?<endTime>(?:[01]?[0-9]|2[0-3]):(?:[0-5][0-9]))/mu;
 
+	let returnedError = false;
 	get('/lektier').then(data => {
 		data.forEach((lektie, index) => {
 			const timeMatch = timeRegex.exec(lektie.aktivitet.tidspunkt);
@@ -49,6 +50,8 @@
 		});
 		$lektier = data;
 		ready = true;
+	}).catch(() => {
+		returnedError = true;
 	});
 
 	function seperateAssignmentsByDate() {
@@ -59,6 +62,21 @@
 		return datoer;
 	}
 </script>
+
+{#if returnedError && !ready}
+	<div role="alert" class="alert alert-error">
+		<svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+		<div class="">
+			<h1 class="font-black">Der skete en fejl ved hentning af lektier</h1>
+			<p>
+				Dette sker typisk hvis Lectio har lavet en opdatering på deres hjemmeside.
+				Vi har modtaget en automatisk fejlrapport og vil kigge på det hurtigst muligt.
+				Tak for din forståelse.
+			</p>
+		</div>
+		<a href="/forside" class="btn btn-neutral  rounded-full">Gå tilbage til forsiden</a>
+	</div>
+{/if}
 
 {#if ready}
 	<h1 class="heading">Lektier</h1>
