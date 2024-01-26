f5bbf6d05be6f93d49fbe2b13c946c6974ed23e7
victor <victordj587@gmail.com>
2024-01-26 18:58:49 +0100

    besked begin

---
+++ src/routes/besked/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { api, get } from '$lib/js/http.js';
+	import LectioAvatar from '$lib/customComponents/LectioAvatar.svelte';
+	import MarkdownIt from 'markdown-it';
+	import purifier from 'dompurify';
+
+    const { sanitize } = purifier;
+    const md = new MarkdownIt();
+
+    const beskedId = new URLSearchParams(window.location.search).get('id');
+
+    let beskedChain = null;
+    let beskedModtagere = null;
+
+    get(`/besked?id=${beskedId}`).then((res) => {
+        beskedChain = res.beskeder;
+        beskedModtagere = res.modtagere;
+        console.log(res);
+    });
+    
+</script>
+
