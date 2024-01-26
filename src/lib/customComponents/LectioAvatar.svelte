c5640be7b5e61faba5a4b0ad998a8a4872de2d5a
victor <victordj587@gmail.com>
2024-01-26 17:54:35 +0100

    beskeder begin

---
+++ src/lib/customComponents/LectioAvatar.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import IntersectionObserver from 'svelte-intersection-observer';
+	import { api } from '$lib/js/http.js';
+    import * as Avatar from "$lib/components/ui/avatar";
+
+	export let id = '';
+	export let navn = '';
+
+	//export let size = 'w-12';
+
+    let element: HTMLElement | null = null;
+	let source: string | null = null;
+
+	async function getImageSource() {
+		try {
+const response = await fetch(`${api}/profil_billed?id=${id}&fullsize=1`, { headers: { 'lectio-cookie': localStorage.getItem('lectio-cookie') || '' } });
+			if (response.ok) return await response.text();
+			return null;
+		} catch (error) {
+			return null;
+		}
+	}
+</script>
+
+<IntersectionObserver
+	{element}
+	once={true}
+	on:intersect={() => {
+		getImageSource().then(data => {
+			source = data;
+		});
+	}}
+>
+	<div bind:this={element}>
+		<Avatar.Root>
+            <Avatar.Image src={source} />
+            {@const initials = navn.split(' ').map(navn => navn[0]).join('').replace(/[.,()-]/g, '').slice(0, 3)}
+            <Avatar.Fallback>{initials}</Avatar.Fallback>
+        </Avatar.Root>
+	</div>
+</IntersectionObserver>
