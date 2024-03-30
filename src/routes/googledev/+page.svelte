ef7b6a14ae213a8ee4d0efb9ae365bad825019d8
victor <victordj587@gmail.com>
2024-03-30 22:47:36 +0100

    google offline token dev

---
+++ src/routes/googledev/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { goto } from '$app/navigation';
+
+	async function getToken() {
+		const res = await fetch('/gen-offline-google-token');
+		const token = await res.text();
+		console.log(token);
+		return token;
+	}
+
+	getToken().then(token => {
+		goto(token);
+	});
+</script>
