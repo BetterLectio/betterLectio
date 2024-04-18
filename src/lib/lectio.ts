37111e75a79a021577b48998bede95c42bb7f522
Johnny JTH <johanohly@gmail.com>
2024-04-18 13:34:42 +0200

    Update LECTIO_OAUTH_API URL

--- src/lib/lectio.ts
+++ src/lib/lectio.ts
@@ -1,2 +1,1 @@
-// export const LECTIO_OAUTH_API = 'https://oauth-betterlectio.netlify.app';
-export const LECTIO_OAUTH_API = 'http://localhost:5174';
+export const LECTIO_OAUTH_API = 'https://oauth-betterlectio.netlify.app';
