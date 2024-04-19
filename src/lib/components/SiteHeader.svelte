69877a6e0b91be163d5e459a256d5198b81e2a94
Johnny JTH <johanohly@gmail.com>
2024-04-19 17:13:06 +0200

    Better site header (nav bar)

--- src/lib/components/SiteHeader.svelte
+++ src/lib/components/SiteHeader.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-	import Button from '$lib/components/ui/button/button.svelte';
-	import Separator from '$lib/components/ui/separator/separator.svelte';
+	import { Button } from '$lib/components/ui/button';
 
 	import { getCurrent } from '@tauri-apps/api/window';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
@@ -9,12 +8,16 @@
 		Avatar,
 		Copy,
 		Cross1 as Cross,
+		Home,
 		DividerHorizontal as Minus,
 		Moon,
+		Reload,
+		RotateCounterClockwise,
 		Stop as Square,
 		Sun
 	} from 'radix-icons-svelte';
 	import { onMount } from 'svelte';
+	import { Separator } from './ui/separator';
 
 	let fullscreen = false;
 	onMount(async () => (fullscreen = await getCurrent().isMaximized()));
@@ -24,11 +27,20 @@
 
 <svelte:window on:resize={resize} />
 
-<div data-tauri-drag-region class="fixed flex z-50 justify-end w-full titlebar">
-	<div
-		class="flex justify-end bg-background pl-0.5 rounded-bl-lg border border-t-0 border-input shadow-md"
-	>
-		<Button variant="outline" class="my-0.5 h-7 flex justify-between w-48" data-command-opener>
+<header
+	data-tauri-drag-region
+	class="sticky h-[42px] top-0 z-40 w-full border-b border-border bg-background/60 shadow-sm backdrop-blur justify-between flex flex-row"
+>
+	<div class="flex items-center gap-1 px-2 py-1">
+		<Button href="/" variant="ghost" size="icon">
+			<Home class="w-6 h-6" />
+		</Button>
+		<Button on:click={() => location.reload()} variant="ghost" size="icon">
+			<Reload class="w-6 h-6" />
+		</Button>
+	</div>
+	<div class="flex items-center">
+		<Button variant="outline" class="flex justify-between w-[50vw] h-7" data-command-opener>
 			<div>
 				<p class="text-sm text-muted-foreground">Søg...</p>
 			</div>
@@ -46,22 +58,21 @@
 				</kbd>
 			</div>
 		</Button>
-		<Button on:click={toggleMode} variant="ghost" size="icon" class="h-8 rounded-none">
+	</div>
+	<div class="flex items-center gap-2 shrink-0">
+		<Button on:click={toggleMode} variant="ghost" size="icon">
 			<Sun
-				class="w-4 h-4 transition-all duration-200 scale-100 rotate-0 dark:-rotate-90 dark:scale-0"
+				class="w-5 h-5 transition-all duration-200 scale-100 rotate-0 dark:-rotate-90 dark:scale-0"
 			/>
 			<Moon
-				class="absolute w-4 h-4 transition-all duration-200 scale-0 rotate-90 dark:rotate-0 dark:scale-100"
+				class="absolute w-5 h-5 transition-all duration-200 scale-0 rotate-90 dark:rotate-0 dark:scale-100"
 			/>
 			<span class="sr-only">Toggle theme</span>
 		</Button>
-		<!-- <Button variant="ghost" size="icon" class="h-8 rounded-none">
-			<Avatar class="w-5 h-5" />
-		</Button> -->
 		<DropdownMenu.Root>
 			<DropdownMenu.Trigger>
-				<Button variant="ghost" size="icon" class="h-8 rounded-none">
-					<Avatar class="w-5 h-5" />
+				<Button variant="ghost" size="icon">
+					<Avatar class="w-6 h-6" />
 				</Button>
 			</DropdownMenu.Trigger>
 			<DropdownMenu.Content>
@@ -73,40 +84,37 @@
 					<DropdownMenu.Item href="/sync">Google Sync</DropdownMenu.Item>
 					<DropdownMenu.Item>Lektier</DropdownMenu.Item>
 					<DropdownMenu.Item>Dokumenter</DropdownMenu.Item>
-					<Separator orientation="horizontal" class="my-2" />
+					<Separator class="my-2" />
 					<DropdownMenu.Item href="/indstillinger">Indstillinger</DropdownMenu.Item>
 				</DropdownMenu.Group>
 			</DropdownMenu.Content>
 		</DropdownMenu.Root>
-		<Separator orientation="vertical" class="h-8 mx-4" />
-		<Button
-			on:click={() => getCurrent().minimize()}
-			variant="ghost"
-			size="icon"
-			class="h-8 rounded-none"
-		>
-			<Minus class="w-4 h-4" />
-		</Button>
-		<Button
-			on:click={async () => await getCurrent().toggleMaximize()}
-			variant="ghost"
-			size="icon"
-			class="h-8 rounded-none"
-		>
-			{#if fullscreen}
-				<Copy class="w-4 h-4" />
-			{:else}
-				<Square class="w-4 h-4" />
-			{/if}
-		</Button>
-		<Button
-			on:click={() => getCurrent().close()}
-			variant="ghost"
-			size="icon"
-			class="h-8 rounded-none hover:bg-destructive hover:to-destructive-foreground"
-		>
-			<Cross class="w-4 h-4" />
-		</Button>
+		<div>
+			<Button
+				on:click={() => getCurrent().minimize()}
+				variant="ghost"
+				class="h-full rounded-none"
+			>
+				<Minus class="w-5 h-5" />
+			</Button>
+			<Button
+				on:click={async () => await getCurrent().toggleMaximize()}
+				variant="ghost"
+				class="h-full rounded-none"
+			>
+				{#if fullscreen}
+					<Copy class="w-5 h-5" />
+				{:else}
+					<Square class="w-5 h-5" />
+				{/if}
+			</Button>
+			<Button
+				on:click={() => getCurrent().close()}
+				variant="ghost"
+				class="h-full rounded-none hover:bg-destructive hover:text-white"
+			>
+				<Cross class="w-5 h-5" />
+			</Button>
+		</div>
 	</div>
-</div>
-<div class="pb-10"></div>
+</header>
