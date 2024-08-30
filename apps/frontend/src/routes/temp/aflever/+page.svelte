ce25a2039be1ab220624b54954f8cd50933513d5
jona799t <70392256+jona799t@users.noreply.github.com>
2024-08-30 17:55:45 +0200

    tab fix

--- apps/frontend/src/routes/temp/aflever/+page.svelte
+++ apps/frontend/src/routes/temp/aflever/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-    import { authStore } from '$lib/stores';
+	import { authStore } from '$lib/stores';
 
 	const exerciseid = $page.url.searchParams.get('id');
 	if (!exerciseid) alert("Intet opgave id");
 
-    async function uploadOpgave(event: SubmitEvent) {
+	async function uploadOpgave(event: SubmitEvent) {
         const form = event.target as HTMLFormElement;
         const formData = new FormData(form);
         
@@ -20,10 +20,10 @@
 
 
 <div class="page-container">
-    <h1>Opgave aflevering (demo)</h1>
-    <form enctype="multipart/form-data" on:submit|preventDefault={uploadOpgave}>
-        <input name="note" type="text" placeholder="Note" />
-        <input name="opgave" type="file">
-        <button type="submit">Upload</button>
-    </form>
+	<h1>Opgave aflevering (demo)</h1>
+	<form enctype="multipart/form-data" on:submit|preventDefault={uploadOpgave}>
+		<input name="note" type="text" placeholder="Note" />
+		<input name="opgave" type="file">
+		<button type="submit">Upload</button>
+	</form>
 </div>
